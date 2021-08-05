7d7ca57e242ab7d134fec79cf8b5afccb459aed0
Leonard Xu <xbjtdcq@gmail.com>
2021-08-06 00:36:32 +0800

    [doc] Use sphinx-rtd-theme to manage project documents

---
+++ docs/build_docs.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+################################################################################
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+
+set -x
+
+# step-1: install dependencies
+apt-get update
+apt-get -y install git rsync python3-pip python3-sphinx python3-git python3-sphinx-rtd-theme python3-stemmer python3-virtualenv python3-setuptools
+python3 -m pip install myst-parser pygments
+
+export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
+export REPO_NAME="${GITHUB_REPOSITORY##*/}"
+temp_docs_root=`mktemp -d`
+
+# step-2: build sites for all branches(for multiple versioned docs), excludes 'HEAD' and 'gh-pages'
+make -C docs clean
+branches="`git for-each-ref '--format=%(refname:lstrip=-1)' refs/remotes/origin/ | grep -viE '^(HEAD|gh-pages)$'`"
+for current_branch in ${branches}; do
+   export current_branch
+   git checkout ${current_branch}
+
+   # skip the branch that has no docs
+   if [ ! -e 'docs/conf.py' ]; then
+      echo -e "\tINFO: Couldn't find 'docs/conf.py' for branch: ${current_branch}, just skip this branch"
+      continue
+   fi
+   echo "INFO: Building sites for branch: ${current_branch}"
+   sphinx-build -b html docs/ docs/_build/html/${current_branch}
+
+   # copy the build content to temp dir
+   rsync -av "docs/_build/html/" "${temp_docs_root}/"
+
+done
+
+git checkout master
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+
+# step-3: push build sites to gh-pages branch
+pushd "${temp_docs_root}"
+git init
+git remote add deploy "https://token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+git checkout -b gh-pages
+
+touch .nojekyll
+cat > index.html <<EOF
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Flink CDC Connectors</title>
+      <meta http-equiv = "refresh" content="0; url='/${REPO_NAME}/master/'" />
+   </head>
+   <body>
+      <p>Please wait while you're redirected to our <a href="/${REPO_NAME}/master/">documentation</a>.</p>
+   </body>
+</html>
+EOF
+
+git add .
+git commit -m "Generated docs from commit ${GITHUB_SHA}"
+git push deploy gh-pages --force
+
+# pop back and exit
+popd
+exit 0
---
+++ docs/conf.py
@@ -0,0 +1,123 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Maven
+# Build your Java project and run tests with Apache Maven.
+# Add steps that analyze code, save build artifacts, deploy, and more:
+# https://docs.microsoft.com/azure/devops/pipelines/languages/java
+
+# -*- coding: utf-8 -*-
+#
+# Configuration file for the Sphinx documentation builder.
+#
+# This file does only contain a selection of the most common options. For a
+# full list see the documentation:
+# http://www.sphinx-doc.org/en/master/config
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'Flink CDC Connectors'
+copyright = '2021, Leonard Xu'
+author = 'Leonard Xu'
+
+# The full version, including alpha/beta/rc tags
+release = '2.0.0'
+
+
+# -- General configuration ---------------------------------------------------
+
+# If your documentation needs a minimal Sphinx version, state it here.
+#
+# needs_sphinx = '1.0'
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx_rtd_theme',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
+    'myst_parser'
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+
+import myst_parser
+
+source_parsers = {
+    '.md': myst_parser
+}
+source_suffix = ['.md']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+# multiple version supports
+import sys, os
+sys.path.insert(0, os.path.abspath('.'))
+
+try:
+    html_context
+except NameError:
+    html_context = dict()
+html_context['display_lower_left'] = True
+
+if 'REPO_NAME' in os.environ:
+    REPO_NAME = os.environ['REPO_NAME']
+else:
+    REPO_NAME = ''
+
+from git import Repo
+repo = Repo( search_parent_directories=True )
+
+if 'current_version' in os.environ:
+    current_version = os.environ['current_version']
+else:
+    current_version = repo.active_branch.name
+
+html_context['current_version'] = current_version
+html_context['version'] = current_version
+html_context['versions'] = list()
+
+versions = [branch.name for branch in repo.branches]
+for version in versions:
+    html_context['versions'].append( (version, '/' +REPO_NAME+ '/' +version+ '/') )
+
+html_context['display_github'] = True
