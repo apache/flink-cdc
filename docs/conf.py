7ca30b839e3c632702dd86a9528e4fa8e121aefa
Jark Wu <jark@apache.org>
2021-10-26 21:23:50 +0800

    [docs] Add favicon for docs website (#525)

--- docs/conf.py
+++ docs/conf.py
@@ -72,6 +72,11 @@ templates_path = ['_templates']
 #
 html_theme = 'sphinx_rtd_theme'
 
+# The name of an image file (within the static path) to use as favicon of the
+# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+html_favicon = '_static/fig/favicon.png'
+
 import myst_parser
 
 source_parsers = {
