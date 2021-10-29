75fc9fb9f84895fd0986541332a6064fe8b8d1e3
Leonard Xu <xbjtdcq@gmail.com>
2021-10-30 01:19:01 +0800

    [mysql] Introduce Jdbc connection pools

---
+++ flink-connector-mysql-cdc/src/main/java/com/ververica/cdc/connectors/mysql/source/connection/PooledDataSourceFactory.java
@@ -0,0 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ververica.cdc.connectors.mysql.source.connection;
+
+import com.ververica.cdc.connectors.mysql.source.config.MySqlSourceConfig;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+/** A connection pool factory to create pooled DataSource {@link HikariDataSource}. */
+public class PooledDataSourceFactory {
+
+    public static final String JDBC_URL_PATTERN =
+            "jdbc:mysql://%s:%s/?useInformationSchema=true&nullCatalogMeansCurrent=false&useUnicode=true&characterEncoding=UTF-8&characterSetResults=UTF-8&zeroDateTimeBehavior=CONVERT_TO_NULL";
+    public static final String CONNECTION_POOL_PREFIX = "connection-pool-";
+
+    private PooledDataSourceFactory() {}
+
+    public static HikariDataSource createPooledDataSource(MySqlSourceConfig sourceConfig) {
+        final HikariConfig config = new HikariConfig();
+
+        String hostName = sourceConfig.getHostname();
+        int port = sourceConfig.getPort();
+
+        config.setPoolName(CONNECTION_POOL_PREFIX + hostName + ":" + port);
+        config.setJdbcUrl(String.format(JDBC_URL_PATTERN, hostName, port));
+        config.setUsername(sourceConfig.getUsername());
+        config.setPassword(sourceConfig.getPassword());
+        config.setMaximumPoolSize(sourceConfig.getConnectionPoolSize());
+        config.setConnectionTimeout(sourceConfig.getConnectTimeout().toMillis());
+
+        // optional optimization configurations for pooled DataSource
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+
+        return new HikariDataSource(config);
+    }
+}
