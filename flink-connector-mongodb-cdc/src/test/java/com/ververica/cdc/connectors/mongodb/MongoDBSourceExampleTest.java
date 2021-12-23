b1a3e8fb22a80c77b4c9d38c10e25f3cc31bd9b7
wangjiang06 <wangjiang06@58.com>
2021-12-23 21:30:56 +0800

    Add MongoDBSource API example

---
+++ flink-connector-mongodb-cdc/src/test/java/com/ververica/cdc/connectors/mongodb/MongoDBSourceExampleTest.java
@@ -0,0 +1,26 @@
+package com.ververica.cdc.connectors.mongodb;
+
+import com.ververica.cdc.debezium.JsonDebeziumDeserializationSchema;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
+
+public class MongoDBSourceExampleTest {
+
+    public static void main(String[] args) throws Exception {
+        SourceFunction<String> sourceFunction = MongoDBSource.<String>builder()
+                .hosts("127.0.0.1:27017")
+                .username("mongouser")
+                .password("mongopw")
+                .database("mgdb")
+                .collection("orders")
+                .deserializer(new JsonDebeziumDeserializationSchema())
+                .build();
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        env.addSource(sourceFunction)
+                .print().setParallelism(1); // use parallelism 1 for sink to keep message ordering
+
+        env.execute();
+    }
+}
