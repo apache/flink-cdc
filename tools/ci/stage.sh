75c576a54274476cfaff201ea76e6f2cd8f41702
He Wang <wanghechn@qq.com>
2022-03-16 10:21:07 +0800

    remove other stage

--- tools/ci/stage.sh
+++ tools/ci/stage.sh
@@ -16,61 +16,65 @@
 #  See the License for the specific language governing permissions and
 # limitations under the License.
 ################################################################################
-STAGE_MYSQL="mysql"
-STAGE_POSTGRES="postgres"
-STAGE_ORACLE="oracle"
-STAGE_MONGODB="mongodb"
-STAGE_SQLSERVER="sqlserver"
-STAGE_E2E="e2e"
-STAGE_MISC="misc"
-
-MODULES_MYSQL="\
-flink-connector-mysql-cdc,\
-flink-sql-connector-mysql-cdc"
-
-MODULES_POSTGRES="\
-flink-connector-postgres-cdc,\
-flink-sql-connector-postgres-cdc"
-
-MODULES_ORACLE="\
-flink-connector-oracle-cdc,\
-flink-sql-connector-oracle-cdc"
-
-MODULES_MONGODB="\
-flink-connector-mongodb-cdc,\
-flink-sql-connector-mongodb-cdc"
-
-MODULES_SQLSERVER="\
-flink-connector-sqlserver-cdc,\
-flink-sql-connector-sqlserver-cdc"
-
+#STAGE_MYSQL="mysql"
+#STAGE_POSTGRES="postgres"
+#STAGE_ORACLE="oracle"
+#STAGE_MONGODB="mongodb"
+#STAGE_SQLSERVER="sqlserver"
+#STAGE_E2E="e2e"
+#STAGE_MISC="misc"
+#
+#MODULES_MYSQL="\
+#flink-connector-mysql-cdc,\
+#flink-sql-connector-mysql-cdc"
+#
+#MODULES_POSTGRES="\
+#flink-connector-postgres-cdc,\
+#flink-sql-connector-postgres-cdc"
+#
+#MODULES_ORACLE="\
+#flink-connector-oracle-cdc,\
+#flink-sql-connector-oracle-cdc"
+#
+#MODULES_MONGODB="\
+#flink-connector-mongodb-cdc,\
+#flink-sql-connector-mongodb-cdc"
+#
+#MODULES_SQLSERVER="\
+#flink-connector-sqlserver-cdc,\
+#flink-sql-connector-sqlserver-cdc"
+#
 MODULES_E2E="\
 flink-cdc-e2e-tests"
+
+#MODULES_OCEANBASE="\
+#flink-connector-oceanbase-cdc,\
+#flink-sql-connector-oceanbase-cdc"
 
 function get_compile_modules_for_stage() {
     local stage=$1
 
     case ${stage} in
-        (${STAGE_MYSQL})
-            echo "-pl $MODULES_MYSQL -am"
-        ;;
-        (${STAGE_POSTGRES})
-            echo "-pl $MODULES_POSTGRES -am"
-        ;;
-        (${STAGE_ORACLE})
-            echo "-pl $MODULES_ORACLE -am"
-        ;;
-        (${STAGE_MONGODB})
-            echo "-pl $MODULES_MONGODB -am"
-        ;;
-        (${STAGE_SQLSERVER})
-            echo "-pl $MODULES_SQLSERVER -am"
-        ;;
-        (${STAGE_E2E})
-            # compile everything; using the -am switch does not work with negated module lists!
-            # the negation takes precedence, thus not all required modules would be built
-            echo ""
-        ;;
+#        (${STAGE_MYSQL})
+#            echo "-pl $MODULES_MYSQL -am"
+#        ;;
+#        (${STAGE_POSTGRES})
+#            echo "-pl $MODULES_POSTGRES -am"
+#        ;;
+#        (${STAGE_ORACLE})
+#            echo "-pl $MODULES_ORACLE -am"
+#        ;;
+#        (${STAGE_MONGODB})
+#            echo "-pl $MODULES_MONGODB -am"
+#        ;;
+#        (${STAGE_SQLSERVER})
+#            echo "-pl $MODULES_SQLSERVER -am"
+#        ;;
+#        (${STAGE_E2E})
+#            # compile everything; using the -am switch does not work with negated module lists!
+#            # the negation takes precedence, thus not all required modules would be built
+#            echo ""
+#        ;;
         (${STAGE_MISC})
             # compile everything; using the -am switch does not work with negated module lists!
             # the negation takes precedence, thus not all required modules would be built
@@ -82,41 +86,38 @@ function get_compile_modules_for_stage() {
 function get_test_modules_for_stage() {
     local stage=$1
 
-    local modules_mysql=$MODULES_MYSQL
-    local modules_postgres=$MODULES_POSTGRES
-    local modules_oracle=$MODULES_ORACLE
-    local modules_mongodb=$MODULES_MONGODB
-    local modules_sqlserver=$MODULES_SQLSERVER
+#    local modules_mysql=$MODULES_MYSQL
+#    local modules_postgres=$MODULES_POSTGRES
+#    local modules_oracle=$MODULES_ORACLE
+#    local modules_mongodb=$MODULES_MONGODB
+#    local modules_sqlserver=$MODULES_SQLSERVER
     local modules_e2e=$MODULES_E2E
-    local negated_mysql=\!${MODULES_MYSQL//,/,\!}
-    local negated_postgres=\!${MODULES_POSTGRES//,/,\!}
-    local negated_oracle=\!${MODULES_ORACLE//,/,\!}
-    local negated_mongodb=\!${MODULES_MONGODB//,/,\!}
-    local negated_sqlserver=\!${MODULES_SQLSERVER//,/,\!}
-    local negated_e2e=\!${MODULES_E2E//,/,\!}
-    local modules_misc="$negated_mysql,$negated_postgres,$negated_oracle,$negated_mongodb,$negated_sqlserver,$negated_e2e"
-
+#    local negated_mysql=\!${MODULES_MYSQL//,/,\!}
+#    local negated_postgres=\!${MODULES_POSTGRES//,/,\!}
+#    local negated_oracle=\!${MODULES_ORACLE//,/,\!}
+#    local negated_mongodb=\!${MODULES_MONGODB//,/,\!}
+#    local negated_sqlserver=\!${MODULES_SQLSERVER//,/,\!}
+#    local negated_e2e=\!${MODULES_E2E//,/,\!}
+    local modules_misc="$negated_e2e"
+#
     case ${stage} in
-        (${STAGE_MYSQL})
-            echo "-pl $modules_mysql"
-        ;;
-        (${STAGE_POSTGRES})
-            echo "-pl $modules_postgres"
-        ;;
-        (${STAGE_ORACLE})
-            echo "-pl $modules_oracle"
-        ;;
-        (${STAGE_MONGODB})
-            echo "-pl $modules_mongodb"
-        ;;
-        (${STAGE_SQLSERVER})
-            echo "-pl $modules_sqlserver"
-        ;;
+#        (${STAGE_MYSQL})
+#            echo "-pl $modules_mysql"
+#        ;;
+#        (${STAGE_POSTGRES})
+#            echo "-pl $modules_postgres"
+#        ;;
+#        (${STAGE_ORACLE})
+#            echo "-pl $modules_oracle"
+#        ;;
+#        (${STAGE_MONGODB})
+#            echo "-pl $modules_mongodb"
+#        ;;
+#        (${STAGE_SQLSERVER})
+#            echo "-pl $modules_sqlserver"
+#        ;;
         (${STAGE_E2E})
             echo "-pl $modules_e2e"
-        ;;
-        (${STAGE_MISC})
-            echo "-pl $modules_misc"
         ;;
     esac
 }
