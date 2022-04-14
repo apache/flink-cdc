ad80e47c42735c42c9eacf125bd56440b2f85f7f
yurunchuan <yurch@chinatelecom.cn>
2022-04-14 12:57:45 +0800

    [mysql] Avoid duplicate split requests when add new table (#1156)

--- flink-connector-mysql-cdc/src/main/java/com/ververica/cdc/connectors/mysql/source/utils/RecordUtils.java
+++ flink-connector-mysql-cdc/src/main/java/com/ververica/cdc/connectors/mysql/source/utils/RecordUtils.java
@@ -41,6 +41,7 @@ import java.time.Instant;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
