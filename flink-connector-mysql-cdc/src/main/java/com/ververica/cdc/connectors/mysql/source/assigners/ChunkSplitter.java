5a1b23defb9d6a2011e3bcca80d12c722480665d
gongzhongqiang <764629910@qq.com>
2022-04-21 22:36:37 +0800

    [mysql] Fix dead loop when splitting chunks.

--- flink-connector-mysql-cdc/src/main/java/com/ververica/cdc/connectors/mysql/source/assigners/ChunkSplitter.java
+++ flink-connector-mysql-cdc/src/main/java/com/ververica/cdc/connectors/mysql/source/assigners/ChunkSplitter.java
@@ -18,6 +18,7 @@
 
 package com.ververica.cdc.connectors.mysql.source.assigners;
 
+import org.apache.flink.annotation.VisibleForTesting;
 import org.apache.flink.table.types.DataType;
 import org.apache.flink.table.types.logical.LogicalTypeRoot;
 import org.apache.flink.table.types.logical.RowType;
@@ -167,7 +168,8 @@ class ChunkSplitter {
      * Split table into evenly sized chunks based on the numeric min and max value of split column,
      * and tumble chunks in step size.
      */
-    private List<ChunkRange> splitEvenlySizedChunks(
+    @VisibleForTesting
+    public List<ChunkRange> splitEvenlySizedChunks(
             TableId tableId, Object min, Object max, long approximateRowCnt, int chunkSize) {
         LOG.info(
                 "Use evenly-sized chunk optimization for table {}, the approximate row count is {}, the chunk size is {}",
@@ -185,7 +187,12 @@ class ChunkSplitter {
         while (ObjectUtils.compare(chunkEnd, max) <= 0) {
             splits.add(ChunkRange.of(chunkStart, chunkEnd));
             chunkStart = chunkEnd;
-            chunkEnd = ObjectUtils.plus(chunkEnd, chunkSize);
+            try {
+                chunkEnd = ObjectUtils.plus(chunkEnd, chunkSize);
+            } catch (ArithmeticException e) {
+                // Stop chunk split to avoid dead loop when number overflows.
+                break;
+            }
         }
         // add the ending split
         splits.add(ChunkRange.of(chunkStart, null));
