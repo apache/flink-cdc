47e005ab47f0802338222702f2edb27cc66b3bfe
Qingsheng Ren <renqschn@gmail.com>
2022-04-23 09:08:30 +0800

    [mysql] Catch and hold exception in chunk splitter thread and rethrow in getNext() (#1109)

--- flink-connector-mysql-cdc/src/test/resources/ddl/customer.sql
+++ flink-connector-mysql-cdc/src/test/resources/ddl/customer.sql
@@ -137,6 +137,36 @@ VALUES (1,'user_1','Shanghai','123567891234'),
        (3,'user_9','Shanghai','123567891234'),
        (3,'user_10','Shanghai','123567891234');
 
+CREATE TABLE customers_no_pk (
+   id INTEGER NOT NULL,
+   name VARCHAR(255) NOT NULL DEFAULT 'flink',
+   address VARCHAR(1024),
+   phone_number VARCHAR(512)
+);
+
+INSERT INTO customers_no_pk
+VALUES (101,"user_1","Shanghai","123567891234"),
+       (102,"user_2","Shanghai","123567891234"),
+       (103,"user_3","Shanghai","123567891234"),
+       (109,"user_4","Shanghai","123567891234"),
+       (110,"user_5","Shanghai","123567891234"),
+       (111,"user_6","Shanghai","123567891234"),
+       (118,"user_7","Shanghai","123567891234"),
+       (121,"user_8","Shanghai","123567891234"),
+       (123,"user_9","Shanghai","123567891234"),
+       (1009,"user_10","Shanghai","123567891234"),
+       (1010,"user_11","Shanghai","123567891234"),
+       (1011,"user_12","Shanghai","123567891234"),
+       (1012,"user_13","Shanghai","123567891234"),
+       (1013,"user_14","Shanghai","123567891234"),
+       (1014,"user_15","Shanghai","123567891234"),
+       (1015,"user_16","Shanghai","123567891234"),
+       (1016,"user_17","Shanghai","123567891234"),
+       (1017,"user_18","Shanghai","123567891234"),
+       (1018,"user_19","Shanghai","123567891234"),
+       (1019,"user_20","Shanghai","123567891234"),
+       (2000,"user_21","Shanghai","123567891234");
+
 -- table has combined primary key
 CREATE TABLE customer_card (
   card_no BIGINT NOT NULL,
