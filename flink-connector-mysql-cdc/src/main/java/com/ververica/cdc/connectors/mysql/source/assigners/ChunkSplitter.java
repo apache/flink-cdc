96c84d9c65938c7873f33406c72f7ec5612df4cd
Hang Ruan <ruanhang1993@hotmail.com>
2022-08-01 17:58:55 +0800

    [mysql] Generates multiple chunks when approximate row count is bigger than chunk size (#1193)
    This closes #1191.

--- flink-connector-mysql-cdc/src/main/java/com/ververica/cdc/connectors/mysql/source/assigners/ChunkSplitter.java
+++ flink-connector-mysql-cdc/src/main/java/com/ververica/cdc/connectors/mysql/source/assigners/ChunkSplitter.java
@@ -153,7 +153,7 @@ class ChunkSplitter {
                 // the minimum dynamic chunk size is at least 1
                 final int dynamicChunkSize = Math.max((int) (distributionFactor * chunkSize), 1);
                 return splitEvenlySizedChunks(
-                        tableId, min, max, approximateRowCnt, dynamicChunkSize);
+                        tableId, min, max, approximateRowCnt, chunkSize, dynamicChunkSize);
             } else {
                 return splitUnevenlySizedChunks(
                         jdbc, tableId, splitColumnName, min, max, chunkSize);
@@ -169,12 +169,18 @@ class ChunkSplitter {
      */
     @VisibleForTesting
     public List<ChunkRange> splitEvenlySizedChunks(
-            TableId tableId, Object min, Object max, long approximateRowCnt, int chunkSize) {
+            TableId tableId,
+            Object min,
+            Object max,
+            long approximateRowCnt,
+            int chunkSize,
+            int dynamicChunkSize) {
         LOG.info(
-                "Use evenly-sized chunk optimization for table {}, the approximate row count is {}, the chunk size is {}",
+                "Use evenly-sized chunk optimization for table {}, the approximate row count is {}, the chunk size is {}, the dynamic chunk size is {}",
                 tableId,
                 approximateRowCnt,
-                chunkSize);
+                chunkSize,
+                dynamicChunkSize);
         if (approximateRowCnt <= chunkSize) {
             // there is no more than one chunk, return full table as a chunk
             return Collections.singletonList(ChunkRange.all());
@@ -182,12 +188,12 @@ class ChunkSplitter {
 
         final List<ChunkRange> splits = new ArrayList<>();
         Object chunkStart = null;
-        Object chunkEnd = ObjectUtils.plus(min, chunkSize);
+        Object chunkEnd = ObjectUtils.plus(min, dynamicChunkSize);
         while (ObjectUtils.compare(chunkEnd, max) <= 0) {
             splits.add(ChunkRange.of(chunkStart, chunkEnd));
             chunkStart = chunkEnd;
             try {
-                chunkEnd = ObjectUtils.plus(chunkEnd, chunkSize);
+                chunkEnd = ObjectUtils.plus(chunkEnd, dynamicChunkSize);
             } catch (ArithmeticException e) {
                 // Stop chunk split to avoid dead loop when number overflows.
                 break;
