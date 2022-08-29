cf8a2f4b077d5e5e14ac0a6c75d997e247fcd4c7
empcl <1515827454@qq.com>
2022-08-29 10:31:06 +0800

    [hotfix][docs] Fix comment typo in MysqlSnapshotSplit (#1513)
    Co-authored-by: chenlei677 <chenlei677@jd.com>

--- flink-connector-mysql-cdc/src/main/java/com/ververica/cdc/connectors/mysql/source/split/MySqlSnapshotSplit.java
+++ flink-connector-mysql-cdc/src/main/java/com/ververica/cdc/connectors/mysql/source/split/MySqlSnapshotSplit.java
@@ -38,7 +38,7 @@ public class MySqlSnapshotSplit extends MySqlSplit {
 
     @Nullable private final Object[] splitStart;
     @Nullable private final Object[] splitEnd;
-    /** The high watermark is not bull when the split read finished. */
+    /** The high watermark is not null when the split read finished. */
     @Nullable private final BinlogOffset highWatermark;
 
     @Nullable transient byte[] serializedFormCache;
