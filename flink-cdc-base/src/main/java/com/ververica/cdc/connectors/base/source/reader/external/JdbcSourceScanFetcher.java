dfc79db63c3ec0a5f498c836f74b4e30d3f898c5
Hang Ruan <ruanhang1993@hotmail.com>
2022-10-17 17:34:59 +0800

    [cdc-base] Adopt streaming merge during snapshotSplit scan to avoid OOM (#1219)
    Co-authored-by: ehui <374488688@qq.com>

--- flink-cdc-base/src/main/java/com/ververica/cdc/connectors/base/source/reader/external/JdbcSourceScanFetcher.java
+++ flink-cdc-base/src/main/java/com/ververica/cdc/connectors/base/source/reader/external/JdbcSourceScanFetcher.java
@@ -22,10 +22,10 @@ import org.apache.flink.shaded.guava30.com.google.common.util.concurrent.ThreadF
 
 import com.ververica.cdc.connectors.base.source.meta.split.SnapshotSplit;
 import com.ververica.cdc.connectors.base.source.meta.split.SourceSplitBase;
-import com.ververica.cdc.connectors.base.utils.SourceRecordUtils;
 import io.debezium.connector.base.ChangeEventQueue;
 import io.debezium.pipeline.DataChangeEvent;
 import io.debezium.util.SchemaNameAdjuster;
+import org.apache.kafka.connect.data.Struct;
 import org.apache.kafka.connect.source.SourceRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,12 +33,24 @@ import org.slf4j.LoggerFactory;
 import javax.annotation.Nullable;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicBoolean;
+
+import static com.ververica.cdc.connectors.base.utils.SourceRecordUtils.formatMessageTimestamp;
+import static com.ververica.cdc.connectors.base.utils.SourceRecordUtils.getSplitKey;
+import static com.ververica.cdc.connectors.base.utils.SourceRecordUtils.isDataChangeRecord;
+import static com.ververica.cdc.connectors.base.utils.SourceRecordUtils.isEndWatermarkEvent;
+import static com.ververica.cdc.connectors.base.utils.SourceRecordUtils.isHighWatermarkEvent;
+import static com.ververica.cdc.connectors.base.utils.SourceRecordUtils.isLowWatermarkEvent;
+import static com.ververica.cdc.connectors.base.utils.SourceRecordUtils.splitKeyRangeContains;
+import static com.ververica.cdc.connectors.base.utils.SourceRecordUtils.upsertBinlog;
+import static org.apache.flink.util.Preconditions.checkState;
 
 /**
  * Fetcher to fetch data from table split, the split is the snapshot split {@link SnapshotSplit}.
@@ -58,6 +70,7 @@ public class JdbcSourceScanFetcher implements Fetcher<SourceRecord, SourceSplitB
     // task to read snapshot for current split
     private FetchTask<SourceSplitBase> snapshotSplitReadTask;
     private SnapshotSplit currentSnapshotSplit;
+    private SchemaNameAdjuster nameAdjuster;
 
     public JdbcSourceScanFetcher(JdbcSourceFetchTaskContext taskContext, int subtaskId) {
         this.taskContext = taskContext;
@@ -75,6 +88,7 @@ public class JdbcSourceScanFetcher implements Fetcher<SourceRecord, SourceSplitB
         this.snapshotSplitReadTask = fetchTask;
         this.currentSnapshotSplit = fetchTask.getSplit().asSnapshotSplit();
         taskContext.configure(currentSnapshotSplit);
+        this.nameAdjuster = taskContext.getSchemaNameAdjuster();
         this.queue = taskContext.getQueue();
         this.hasNextElement.set(true);
         this.reachEnd.set(false);
@@ -108,31 +122,53 @@ public class JdbcSourceScanFetcher implements Fetcher<SourceRecord, SourceSplitB
             // data input: [low watermark event][snapshot events][high watermark event][binlog
             // events][binlog-end event]
             // data output: [low watermark event][normalized events][high watermark event]
+            boolean reachBinlogStart = false;
             boolean reachBinlogEnd = false;
-            final List<SourceRecord> sourceRecords = new ArrayList<>();
+            SourceRecord lowWatermark = null;
+            SourceRecord highWatermark = null;
+            Map<Struct, SourceRecord> snapshotRecords = new HashMap<>();
             while (!reachBinlogEnd) {
                 checkReadException();
                 List<DataChangeEvent> batch = queue.poll();
                 for (DataChangeEvent event : batch) {
-                    sourceRecords.add(event.getRecord());
-                    if (SourceRecordUtils.isEndWatermarkEvent(event.getRecord())) {
+                    SourceRecord record = event.getRecord();
+                    if (lowWatermark == null) {
+                        lowWatermark = record;
+                        assertLowWatermark(lowWatermark);
+                        continue;
+                    }
+
+                    if (highWatermark == null && isHighWatermarkEvent(record)) {
+                        highWatermark = record;
+                        // snapshot events capture end and begin to capture binlog events
+                        reachBinlogStart = true;
+                        continue;
+                    }
+
+                    if (reachBinlogStart && isEndWatermarkEvent(record)) {
+                        // capture to end watermark events, stop the loop
                         reachBinlogEnd = true;
                         break;
+                    }
+
+                    if (!reachBinlogStart) {
+                        snapshotRecords.put((Struct) record.key(), record);
+                    } else {
+                        if (isRequiredBinlogRecord(record)) {
+                            // upsert binlog events through the record key
+                            upsertBinlog(snapshotRecords, record);
+                        }
                     }
                 }
             }
             // snapshot split return its data once
             hasNextElement.set(false);
-            return SourceRecordUtils.normalizedSplitRecords(
-                            currentSnapshotSplit,
-                            sourceRecords,
-                            new SchemaNameAdjuster() {
-                                @Override
-                                public String adjust(String proposedName) {
-                                    return null;
-                                }
-                            })
-                    .iterator();
+
+            final List<SourceRecord> normalizedRecords = new ArrayList<>();
+            normalizedRecords.add(lowWatermark);
+            normalizedRecords.addAll(formatMessageTimestamp(snapshotRecords.values()));
+            normalizedRecords.add(highWatermark);
+            return normalizedRecords.iterator();
         }
         // the data has been polled, no more data
         reachEnd.compareAndSet(false, true);
@@ -151,4 +187,22 @@ public class JdbcSourceScanFetcher implements Fetcher<SourceRecord, SourceSplitB
 
     @Override
     public void close() {}
+
+    private void assertLowWatermark(SourceRecord lowWatermark) {
+        checkState(
+                isLowWatermarkEvent(lowWatermark),
+                String.format(
+                        "The first record should be low watermark signal event, but actual is %s",
+                        lowWatermark));
+    }
+
+    private boolean isRequiredBinlogRecord(SourceRecord record) {
+        if (isDataChangeRecord(record)) {
+            Object[] key =
+                    getSplitKey(currentSnapshotSplit.getSplitKeyType(), record, nameAdjuster);
+            return splitKeyRangeContains(
+                    key, currentSnapshotSplit.getSplitStart(), currentSnapshotSplit.getSplitEnd());
+        }
+        return false;
+    }
 }
