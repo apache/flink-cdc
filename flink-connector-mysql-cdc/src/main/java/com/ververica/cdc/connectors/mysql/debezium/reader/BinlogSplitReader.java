b2db96b1c17a132168d6dfe1995f03406069ce3d
Leonard Xu <xbjtdcq@gmail.com>
2023-02-20 15:50:36 +0800

    [hotfix][base][tidb][mysql] Fix wrong condition for execution timeout

--- flink-connector-mysql-cdc/src/main/java/com/ververica/cdc/connectors/mysql/debezium/reader/BinlogSplitReader.java
+++ flink-connector-mysql-cdc/src/main/java/com/ververica/cdc/connectors/mysql/debezium/reader/BinlogSplitReader.java
@@ -193,7 +193,7 @@ public class BinlogSplitReader implements DebeziumReader<SourceRecords, MySqlSpl
             currentTaskRunning = false;
             if (executorService != null) {
                 executorService.shutdown();
-                if (executorService.awaitTermination(READER_CLOSE_TIMEOUT, TimeUnit.SECONDS)) {
+                if (!executorService.awaitTermination(READER_CLOSE_TIMEOUT, TimeUnit.SECONDS)) {
                     LOG.warn(
                             "Failed to close the binlog split reader in {} seconds.",
                             READER_CLOSE_TIMEOUT);
