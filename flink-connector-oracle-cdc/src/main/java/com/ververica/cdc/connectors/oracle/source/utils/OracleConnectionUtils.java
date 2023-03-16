a962714d718a9f67a11c9358a87abcc435043186
Gandalf-liu <894290213@qq.com>
2023-03-16 11:37:49 +0800

    优化oracle 查询tableId

--- flink-connector-oracle-cdc/src/main/java/com/ververica/cdc/connectors/oracle/source/utils/OracleConnectionUtils.java
+++ flink-connector-oracle-cdc/src/main/java/com/ververica/cdc/connectors/oracle/source/utils/OracleConnectionUtils.java
@@ -92,8 +92,8 @@ public class OracleConnectionUtils {
                 new StringBuilder(
                         "SELECT OWNER ,TABLE_NAME,TABLESPACE_NAME FROM ALL_TABLES \n"
                                 + "WHERE TABLESPACE_NAME IS NOT NULL AND TABLESPACE_NAME NOT IN ('SYSTEM','SYSAUX')");
-        String schema = jdbcConnection.connection().getSchema();
         if (tableList != null && !tableList.isEmpty()) {
+            String schema = jdbcConnection.connection().getSchema();
             StringJoiner stringJoiner = new StringJoiner(",");
             for (String tableId : tableList) {
                 String tableName = tableId.replaceFirst(schema + "\\.", "");
