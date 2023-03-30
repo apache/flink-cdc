9264b3e1b245e1fe3d45e4b8ca1ca740e430e2e6
He Wang <wanghechn@qq.com>
2023-03-30 16:07:23 +0800

    update oceanbase and oblogproxy docker image

--- docs/content/快速上手/oceanbase-tutorial-zh.md
+++ docs/content/快速上手/oceanbase-tutorial-zh.md
@@ -16,13 +16,11 @@
 version: '2.1'
 services:
   observer:
-    image: oceanbase/oceanbase-ce:3.1.4
+    image: oceanbase/oceanbase-ce:4.0.0.0
     container_name: observer
-    environment:
-      - 'OB_ROOT_PASSWORD=pswd'
     network_mode: "host"
   oblogproxy:
-    image: whhe/oblogproxy:1.0.3
+    image: whhe/oblogproxy:1.1.0_4x
     container_name: oblogproxy
     environment:
       - 'OB_SYS_USERNAME=root'
@@ -61,12 +59,42 @@ services:
 docker-compose up -d
 ```
 
-### 准备数据：
+### 设置密码
 
-使用新创建的用户名和密码进行登陆。
+OceanBase 中 root 用户默认是没有密码的，但是 oblogproxy 需要配置一个使用非空密码的系统租户用户，因此这里我们需要先为 root@sys 用户设置一个密码。
+
+登陆 sys 租户的 root 用户：
 
 ```shell
-docker-compose exec observer obclient -h127.0.0.1 -P2881 -uroot -ppswd
+docker-compose exec observer obclient -h127.0.0.1 -P2881 -uroot@sys
+```
+
+设置密码，注意这里的密码需要与上一步中 oblogproxy 服务的环境变量 'OB_SYS_PASSWORD' 保持一样。
+
+```mysql
+ALTER USER root IDENTIFIED BY 'pswd';
+```
+
+OceanBase 从社区版 4.0.0.0 开始只支持对非 sys 租户的增量数据拉取，这里我们使用 test 租户的 root 用户作为示例。
+
+登陆 test 租户的 root 用户：
+
+```shell
+docker-compose exec observer obclient -h127.0.0.1 -P2881 -uroot@test
+```
+
+设置密码:
+
+```mysql
+ALTER USER root IDENTIFIED BY 'test';
+```
+
+### 准备数据
+
+使用 'root@test' 用户登陆。
+
+```shell
+docker-compose exec observer obclient -h127.0.0.1 -P2881 -uroot@test -ptest
 ```
 
 ```sql
@@ -134,9 +162,9 @@ Flink SQL> CREATE TABLE orders (
  ) WITH (
     'connector' = 'oceanbase-cdc',
     'scan.startup.mode' = 'initial',
-    'username' = 'root',
-    'password' = 'pswd',
-    'tenant-name' = 'sys',
+    'username' = 'root@test',
+    'password' = 'test',
+    'tenant-name' = 'test',
     'database-name' = '^ob$',
     'table-name' = '^orders$',
     'hostname' = 'localhost',
@@ -156,9 +184,9 @@ Flink SQL> CREATE TABLE products (
   ) WITH (
     'connector' = 'oceanbase-cdc',
     'scan.startup.mode' = 'initial',
-    'username' = 'root',
-    'password' = 'pswd',
-    'tenant-name' = 'sys',
+    'username' = 'root@test',
+    'password' = 'test',
+    'tenant-name' = 'test',
     'database-name' = '^ob$',
     'table-name' = '^products$',
     'hostname' = 'localhost',
--- flink-connector-oceanbase-cdc/src/test/java/com/ververica/cdc/connectors/oceanbase/table/OceanBaseConnectorITCase.java
+++ flink-connector-oceanbase-cdc/src/test/java/com/ververica/cdc/connectors/oceanbase/table/OceanBaseConnectorITCase.java
@@ -268,16 +268,36 @@ public class OceanBaseConnectorITCase extends OceanBaseTestBase {
 
         List<String> expected =
                 Arrays.asList(
-                        "+I(sys,inventory_meta,products,101,scooter,Small 2-wheel scooter,3.1400000000)",
-                        "+I(sys,inventory_meta,products,102,car battery,12V car battery,8.1000000000)",
-                        "+I(sys,inventory_meta,products,103,12-pack drill bits,12-pack of drill bits with sizes ranging from #40 to #3,0.8000000000)",
-                        "+I(sys,inventory_meta,products,104,hammer,12oz carpenter's hammer,0.7500000000)",
-                        "+I(sys,inventory_meta,products,105,hammer,14oz carpenter's hammer,0.8750000000)",
-                        "+I(sys,inventory_meta,products,106,hammer,16oz carpenter's hammer,1.0000000000)",
-                        "+I(sys,inventory_meta,products,107,rocks,box of assorted rocks,5.3000000000)",
-                        "+I(sys,inventory_meta,products,108,jacket,water resistent black wind breaker,0.1000000000)",
-                        "+I(sys,inventory_meta,products,109,spare tire,24 inch spare tire,22.2000000000)",
-                        "+U(sys,inventory_meta,products,106,hammer,18oz carpenter hammer,1.0000000000)");
+                        "+I("
+                                + getTenant()
+                                + ",inventory_meta,products,101,scooter,Small 2-wheel scooter,3.1400000000)",
+                        "+I("
+                                + getTenant()
+                                + ",inventory_meta,products,102,car battery,12V car battery,8.1000000000)",
+                        "+I("
+                                + getTenant()
+                                + ",inventory_meta,products,103,12-pack drill bits,12-pack of drill bits with sizes ranging from #40 to #3,0.8000000000)",
+                        "+I("
+                                + getTenant()
+                                + ",inventory_meta,products,104,hammer,12oz carpenter's hammer,0.7500000000)",
+                        "+I("
+                                + getTenant()
+                                + ",inventory_meta,products,105,hammer,14oz carpenter's hammer,0.8750000000)",
+                        "+I("
+                                + getTenant()
+                                + ",inventory_meta,products,106,hammer,16oz carpenter's hammer,1.0000000000)",
+                        "+I("
+                                + getTenant()
+                                + ",inventory_meta,products,107,rocks,box of assorted rocks,5.3000000000)",
+                        "+I("
+                                + getTenant()
+                                + ",inventory_meta,products,108,jacket,water resistent black wind breaker,0.1000000000)",
+                        "+I("
+                                + getTenant()
+                                + ",inventory_meta,products,109,spare tire,24 inch spare tire,22.2000000000)",
+                        "+U("
+                                + getTenant()
+                                + ",inventory_meta,products,106,hammer,18oz carpenter hammer,1.0000000000)");
         List<String> actual = TestValuesTableFactory.getRawResults("sink");
         assertContainsInAnyOrder(expected, actual);
         result.getJobClient().get().cancel().get();
