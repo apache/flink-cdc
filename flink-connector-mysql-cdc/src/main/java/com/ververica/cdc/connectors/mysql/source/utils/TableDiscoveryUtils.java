ddb033ad1c0a13fb4984486cf3e4c9e9bac8da16
liuliu <liuliu@dtstack.com>
2023-05-29 19:57:21 +0800

    [minor][mysql] Filter databases that do not need to be read

--- flink-connector-mysql-cdc/src/main/java/com/ververica/cdc/connectors/mysql/source/utils/TableDiscoveryUtils.java
+++ flink-connector-mysql-cdc/src/main/java/com/ververica/cdc/connectors/mysql/source/utils/TableDiscoveryUtils.java
@@ -57,7 +57,15 @@ public class TableDiscoveryUtils {
                 "SHOW DATABASES",
                 rs -> {
                     while (rs.next()) {
-                        databaseNames.add(rs.getString(1));
+                        String databaseName = rs.getString(1);
+                        if (tableFilters.databaseFilter().test(databaseName)) {
+                            databaseNames.add(databaseName);
+                            LOG.info(
+                                    "\t including database '{}' for further processing",
+                                    databaseName);
+                        } else {
+                            LOG.info("\t '{}' is filtered out of database capturing", databaseName);
+                        }
                     }
                 });
         LOG.info("\t list of available databases is: {}", databaseNames);
@@ -79,9 +87,11 @@ public class TableDiscoveryUtils {
                                 TableId tableId = new TableId(dbName, null, rs.getString(1));
                                 if (tableFilters.dataCollectionFilter().isIncluded(tableId)) {
                                     capturedTableIds.add(tableId);
-                                    LOG.info("\t including '{}' for further processing", tableId);
+                                    LOG.info(
+                                            "\t including table '{}' for further processing",
+                                            tableId);
                                 } else {
-                                    LOG.info("\t '{}' is filtered out of capturing", tableId);
+                                    LOG.info("\t '{}' is filtered out of table capturing", tableId);
                                 }
                             }
                         });
