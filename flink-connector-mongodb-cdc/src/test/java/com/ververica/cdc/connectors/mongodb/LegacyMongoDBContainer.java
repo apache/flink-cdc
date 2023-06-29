72d093959ea9d461e352d54965ffd0003b7e81d9
yuxiqian <34335406+yuxiqian@users.noreply.github.com>
2023-06-29 09:28:10 +0800

    [fix] add missing license headers

--- flink-connector-mongodb-cdc/src/test/java/com/ververica/cdc/connectors/mongodb/LegacyMongoDBContainer.java
+++ flink-connector-mongodb-cdc/src/test/java/com/ververica/cdc/connectors/mongodb/LegacyMongoDBContainer.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 Ververica Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.ververica.cdc.connectors.mongodb;
 
 import com.github.dockerjava.api.command.InspectContainerResponse;
@@ -157,11 +173,6 @@ public class LegacyMongoDBContainer extends GenericContainer<LegacyMongoDBContai
                         ShardingClusterRole.SHARD.replicaSetName,
                         ShardingClusterRole.SHARD.hostname,
                         MONGODB_PORT));
-    }
-
-    /** Executes a mongo command file. */
-    public String executeCommandFile(String fileNameIgnoreSuffix) {
-        return executeCommandFileInDatabase(fileNameIgnoreSuffix, fileNameIgnoreSuffix);
     }
 
     /** Executes a mongo command file in separate database. */
