6a3b84823ff98340e283ab77817a2d847ceca2d7
yuxiqian <34335406+yuxiqian@users.noreply.github.com>
2023-07-12 15:08:05 +0800

    [mongodb] Add class visibility annotations

--- flink-connector-mongodb-cdc/src/main/java/com/ververica/cdc/connectors/mongodb/table/MongoDBConnectorFullChangelogDeserializationSchema.java
+++ flink-connector-mongodb-cdc/src/main/java/com/ververica/cdc/connectors/mongodb/table/MongoDBConnectorFullChangelogDeserializationSchema.java
@@ -16,6 +16,8 @@
 
 package com.ververica.cdc.connectors.mongodb.table;
 
+import org.apache.flink.annotation.Experimental;
+import org.apache.flink.annotation.PublicEvolving;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.table.data.GenericRowData;
 import org.apache.flink.table.data.RowData;
@@ -37,6 +39,8 @@ import java.time.ZoneId;
  * Deserialization schema from Mongodb ChangeStreamDocument to Flink Table/SQL internal data that
  * produces full changelog mode structure {@link RowData}.
  */
+@Experimental
+@PublicEvolving
 public class MongoDBConnectorFullChangelogDeserializationSchema
         extends MongoDBConnectorDeserializationSchema {
 
