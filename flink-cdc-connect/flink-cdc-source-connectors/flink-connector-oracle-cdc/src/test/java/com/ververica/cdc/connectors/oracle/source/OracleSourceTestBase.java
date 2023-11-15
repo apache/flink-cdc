4da3a5b17252f6458897d67fb76f408c1455b532
Leonard Xu <leonard@apache.org>
2023-11-16 00:01:38 +0800

    [oracle] Skip unsupported nested table for legacy debezium source function

--- flink-cdc-connect/flink-cdc-source-connectors/flink-connector-oracle-cdc/src/test/java/com/ververica/cdc/connectors/oracle/source/OracleSourceTestBase.java
+++ flink-cdc-connect/flink-cdc-source-connectors/flink-connector-oracle-cdc/src/test/java/com/ververica/cdc/connectors/oracle/source/OracleSourceTestBase.java
@@ -65,9 +65,10 @@ public class OracleSourceTestBase extends TestLogger {
     public static final String ORACLE_DATABASE = "ORCLCDB";
     public static final String ORACLE_SCHEMA = "DEBEZIUM";
     public static final String CONNECTOR_USER = "dbzuser";
+    public static final String CONNECTOR_PWD = "dbz";
     public static final String TEST_USER = "debezium";
     public static final String TEST_PWD = "dbz";
-    public static final String CONNECTOR_PWD = "dbz";
+    public static final String TOP_SECRET = "top_secret";
 
     public static final OracleContainer ORACLE_CONTAINER =
             new OracleContainer(
@@ -119,6 +120,11 @@ public class OracleSourceTestBase extends TestLogger {
 
     public static Connection getJdbcConnection() throws SQLException {
         return DriverManager.getConnection(ORACLE_CONTAINER.getJdbcUrl(), TEST_USER, TEST_PWD);
+    }
+
+    public static Connection getJdbcConnectionAsDBA() throws SQLException {
+        return DriverManager.getConnection(
+                ORACLE_CONTAINER.getJdbcUrl(), "sys as sysdba", TOP_SECRET);
     }
 
     public static void createAndInitialize(String sqlFile) throws Exception {
