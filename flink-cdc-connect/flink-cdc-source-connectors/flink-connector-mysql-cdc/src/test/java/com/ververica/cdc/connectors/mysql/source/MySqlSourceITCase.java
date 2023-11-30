26d74c5e6d380fc338063fb3a07365684f9dadc7
Hongshun Wang <loserwang1024@gmail.com>
2023-11-30 10:50:07 +0800

    [mysql] Expose ability to skip backfill in snapshot period for Mysql CDC.

--- flink-cdc-connect/flink-cdc-source-connectors/flink-connector-mysql-cdc/src/test/java/com/ververica/cdc/connectors/mysql/source/MySqlSourceITCase.java
+++ flink-cdc-connect/flink-cdc-source-connectors/flink-connector-mysql-cdc/src/test/java/com/ververica/cdc/connectors/mysql/source/MySqlSourceITCase.java
@@ -351,9 +351,10 @@ public class MySqlSourceITCase extends MySqlSourceTestBase {
     }
 
     @Test
-    public void testEnableBackfillWithPreHighWaterMark() throws Exception {
+    public void testEnableBackfillWithDMLPreHighWaterMark() throws Exception {
 
-        List<String> records = getResultOfWithHooks(false, 21, USE_PRE_HIGHWATERMARK_HOOK);
+        List<String> records =
+                getResultWithDMLInSnapshotPhase(false, 21, USE_PRE_HIGHWATERMARK_HOOK);
 
         List<String> expectedRecords =
                 Arrays.asList(
@@ -384,9 +385,10 @@ public class MySqlSourceITCase extends MySqlSourceTestBase {
     }
 
     @Test
-    public void testEnableBackfillWithPostLowWaterMark() throws Exception {
+    public void testEnableBackfillWithDMLPostLowWaterMark() throws Exception {
 
-        List<String> records = getResultOfWithHooks(false, 21, USE_POST_LOWWATERMARK_HOOK);
+        List<String> records =
+                getResultWithDMLInSnapshotPhase(false, 21, USE_POST_LOWWATERMARK_HOOK);
 
         List<String> expectedRecords =
                 Arrays.asList(
@@ -417,9 +419,10 @@ public class MySqlSourceITCase extends MySqlSourceTestBase {
     }
 
     @Test
-    public void testSkipBackfillWithPreHighWaterMark() throws Exception {
+    public void testSkipBackfillWithDMLPreHighWaterMark() throws Exception {
 
-        List<String> records = getResultOfWithHooks(true, 25, USE_PRE_HIGHWATERMARK_HOOK);
+        List<String> records =
+                getResultWithDMLInSnapshotPhase(true, 25, USE_PRE_HIGHWATERMARK_HOOK);
 
         List<String> expectedRecords =
                 Arrays.asList(
@@ -454,9 +457,10 @@ public class MySqlSourceITCase extends MySqlSourceTestBase {
     }
 
     @Test
-    public void testSkipBackfillWithPostLowWaterMark() throws Exception {
+    public void testSkipBackfillWithDMLPostLowWaterMark() throws Exception {
 
-        List<String> records = getResultOfWithHooks(true, 25, USE_POST_LOWWATERMARK_HOOK);
+        List<String> records =
+                getResultWithDMLInSnapshotPhase(true, 25, USE_POST_LOWWATERMARK_HOOK);
 
         List<String> expectedRecords =
                 Arrays.asList(
@@ -491,7 +495,7 @@ public class MySqlSourceITCase extends MySqlSourceTestBase {
         assertEqualsInAnyOrder(expectedRecords, records);
     }
 
-    private List<String> getResultOfWithHooks(
+    private List<String> getResultWithDMLInSnapshotPhase(
             boolean skipSnapshotBackfill, int fetchSize, int hookType) throws Exception {
         customDatabase.createAndInitialize();
         TestTable customerTable =
