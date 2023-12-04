08c5dfed83f370a349e54f9be20f93d950a31777
Jiabao Sun <jiabao.sun@xtransfer.cn>
2023-12-04 23:06:11 +0800

    [cdc-pipeline-connector][mysql] Add tests for mysql pipeline connector
    Co-authored-by: Hang Ruan <ruanhang1993@hotmail.com>

---
+++ flink-cdc-connect/flink-cdc-pipeline-connectors/flink-cdc-pipeline-connector-mysql/src/test/java/com/ververica/cdc/connectors/mysql/testutils/MySqSourceTestUtils.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2023 Ververica Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ververica.cdc.connectors.mysql.testutils;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+
+/** Test utilities for MySQL event source. */
+public class MySqSourceTestUtils {
+
+    public static final String TEST_USER = "mysqluser";
+    public static final String TEST_PASSWORD = "mysqlpw";
+
+    public static <T> List<T> fetchResults(Iterator<T> iter, int size) {
+        List<T> result = new ArrayList<>(size);
+        while (size > 0 && iter.hasNext()) {
+            T event = iter.next();
+            result.add(event);
+            size--;
+        }
+        return result;
+    }
+
+    public static String getServerId(int parallelism) {
+        final Random random = new Random();
+        int serverId = random.nextInt(100) + 5400;
+        return serverId + "-" + (serverId + parallelism);
+    }
+
+    private MySqSourceTestUtils() {}
+}
