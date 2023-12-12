3a97f876d4e13dbfacb4a77cc45c4b56a23813f4
joyCurry30 <curry30play@gmail.com>
2023-12-12 16:22:10 +0800

    [test] test

--- flink-cdc-debug/src/main/java/com/ververica/cdc/debug/DebugEntry.java
+++ flink-cdc-debug/src/main/java/com/ververica/cdc/debug/DebugEntry.java
@@ -4,7 +4,7 @@ import com.ververica.cdc.cli.CliFrontend;
 
 public class DebugEntry {
     public static void main(String[] args) throws Exception {
-        String jobPath = "/Users/lapata/clickzetta_jobs/flink-cdc-3.0/mysqlToDoris.yml";
+        String jobPath = "test";
         String[] extArgs = {jobPath, "--use-mini-cluster", "true"};
 
         CliFrontend.main(extArgs);
