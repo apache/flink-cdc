2c5e06ec381dcc0e5fc818a176d43cfe45883154
AidenPerce <lp956745128@gmail.com>
2024-01-05 09:09:39 +0800

    rebase from ververica to apache

--- flink-cdc-connect/flink-cdc-source-connectors/flink-connector-mysql-cdc/src/test/java/org/apache/flink/cdc/connectors/mysql/source/utils/ObjectUtilsTest.java
+++ flink-cdc-connect/flink-cdc-source-connectors/flink-connector-mysql-cdc/src/test/java/org/apache/flink/cdc/connectors/mysql/source/utils/ObjectUtilsTest.java
@@ -17,12 +17,55 @@
 
 package org.apache.flink.cdc.connectors.mysql.source.utils;
 
+import com.ververica.cdc.connectors.mysql.debezium.DebeziumUtils;
+import com.ververica.cdc.connectors.mysql.source.MySqlSourceTestBase;
+import com.ververica.cdc.connectors.mysql.source.config.MySqlSourceConfig;
+import com.ververica.cdc.connectors.mysql.source.config.MySqlSourceConfigFactory;
+import com.ververica.cdc.connectors.mysql.testutils.UniqueDatabase;
+import io.debezium.connector.mysql.MySqlConnection;
+import io.debezium.relational.TableId;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 import java.math.BigDecimal;
+import java.sql.SQLException;
+import java.time.ZoneId;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 import static org.junit.Assert.assertEquals;
 
+/** Tests for {@link ObjectUtils}. */
+public class ObjectUtilsTest extends MySqlSourceTestBase {
+
+    private static final UniqueDatabase customerDatabase =
+            new UniqueDatabase(MYSQL_CONTAINER, "customer", "mysqluser", "mysqlpw");
+    private static MySqlConnection mySqlConnection;
+
+    @BeforeClass
+    public static void init() throws SQLException {
+        customerDatabase.createAndInitialize();
+        Map<String, String> properties = new HashMap<>();
+        properties.put("database.hostname", MYSQL_CONTAINER.getHost());
+        properties.put("database.port", String.valueOf(MYSQL_CONTAINER.getDatabasePort()));
+        properties.put("database.user", customerDatabase.getUsername());
+        properties.put("database.password", customerDatabase.getPassword());
+        properties.put("database.serverTimezone", ZoneId.of("UTC").toString());
+        io.debezium.config.Configuration configuration =
+                io.debezium.config.Configuration.from(properties);
+        mySqlConnection = DebeziumUtils.createMySqlConnection(configuration, new Properties());
+    }
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+        if (mySqlConnection != null) {
+            mySqlConnection.close();
+        }
+    }
 /** Tests for {@link org.apache.flink.cdc.connectors.mysql.source.utils.ObjectUtils}. */
 public class ObjectUtilsTest {
 
@@ -41,5 +84,61 @@ public class ObjectUtilsTest {
         assertEquals(
                 new BigDecimal("99.12344"),
                 ObjectUtils.minus(new BigDecimal("100.12345"), new BigDecimal("1.00001")));
+    }
+
+    @Test
+    public void testDbCompare() throws SQLException {
+        TableId tableId =
+                new TableId(
+                        customerDatabase.getDatabaseName(),
+                        null,
+                        "unevenly_shopping_cart_utf8_bin");
+        int resultBin1 =
+                ObjectUtils.dbCompare("aaaa", "zzzz", mySqlConnection, tableId, "pk_varchar");
+        int resultBin2 =
+                ObjectUtils.dbCompare("zzzz", "ZZZZ", mySqlConnection, tableId, "pk_varchar");
+        int resultBin3 = ObjectUtils.dbCompare("9", "a", mySqlConnection, tableId, "pk_varchar");
+        Assertions.assertEquals(-1, resultBin1);
+        Assertions.assertEquals(1, resultBin2);
+        Assertions.assertEquals(-1, resultBin3);
+
+        TableId tableId2 =
+                new TableId(
+                        customerDatabase.getDatabaseName(), null, "unevenly_shopping_cart_utf8_ci");
+        int resultCi1 =
+                ObjectUtils.dbCompare("aaaa", "zzzz", mySqlConnection, tableId2, "pk_varchar");
+        int resultCi2 =
+                ObjectUtils.dbCompare("zzzz", "ZZZZ", mySqlConnection, tableId2, "pk_varchar");
+        int resultCi3 =
+                ObjectUtils.dbCompare("ZZZZ", "aaaa", mySqlConnection, tableId2, "pk_varchar");
+        int resultCi4 = ObjectUtils.dbCompare("9", "a", mySqlConnection, tableId2, "pk_varchar");
+        Assertions.assertEquals(-1, resultCi1);
+        Assertions.assertEquals(0, resultCi2);
+        Assertions.assertEquals(1, resultCi3);
+        Assertions.assertEquals(-1, resultCi4);
+    }
+
+    public static MySqlSourceConfig getConfig(
+            UniqueDatabase database,
+            String[] captureTables,
+            int splitSize,
+            boolean skipSnapshotBackfill) {
+        String[] captureTableIds =
+                Arrays.stream(captureTables)
+                        .map(tableName -> database.getDatabaseName() + "." + tableName)
+                        .toArray(String[]::new);
+
+        return new MySqlSourceConfigFactory()
+                .databaseList(database.getDatabaseName())
+                .tableList(captureTableIds)
+                .serverId("1001-1002")
+                .hostname(MYSQL_CONTAINER.getHost())
+                .port(MYSQL_CONTAINER.getDatabasePort())
+                .username(database.getUsername())
+                .splitSize(splitSize)
+                .fetchSize(2)
+                .password(database.getPassword())
+                .skipSnapshotBackfill(skipSnapshotBackfill)
+                .createConfig(0);
     }
 }
