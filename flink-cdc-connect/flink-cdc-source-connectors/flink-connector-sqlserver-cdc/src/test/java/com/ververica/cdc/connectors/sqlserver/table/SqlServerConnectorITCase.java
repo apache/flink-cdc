89d68d78800dcecdc5d83dc903dfedd718ea5f3f
e-mhui <emhui@foxmail.com>
2024-02-21 14:25:24 +0800

    [Oracle] Add SqlServer UT

--- flink-cdc-connect/flink-cdc-source-connectors/flink-connector-sqlserver-cdc/src/test/java/com/ververica/cdc/connectors/sqlserver/table/SqlServerConnectorITCase.java
+++ flink-cdc-connect/flink-cdc-source-connectors/flink-connector-sqlserver-cdc/src/test/java/com/ververica/cdc/connectors/sqlserver/table/SqlServerConnectorITCase.java
@@ -173,20 +173,20 @@ public class SqlServerConnectorITCase extends SqlServerTestBase {
          * The final database table looks like this:
          *
          * > SELECT * FROM products;
-         * +-----+--------------------+---------------------------------------------------------+--------+
-         * | id  | name               | description                                             | weight |
-         * +-----+--------------------+---------------------------------------------------------+--------+
-         * | 101 | scooter            | Small 2-wheel scooter                                   |   3.14 |
-         * | 102 | car battery        | 12V car battery                                         |    8.1 |
-         * | 103 | 12-pack drill bits | 12-pack of drill bits with sizes ranging from #40 to #3 |    0.8 |
-         * | 104 | hammer             | 12oz carpenter's hammer                                 |   0.75 |
-         * | 105 | hammer             | 14oz carpenter's hammer                                 |  0.875 |
-         * | 106 | hammer             | 18oz carpenter hammer                                   |      1 |
-         * | 107 | rocks              | box of assorted rocks                                   |    5.1 |
-         * | 108 | jacket             | water resistent black wind breaker                      |    0.1 |
-         * | 109 | spare tire         | 24 inch spare tire                                      |   22.2 |
-         * | 110 | jacket             | new water resistent white wind breaker                  |    0.5 |
-         * +-----+--------------------+---------------------------------------------------------+--------+
+         * +-----+--------------------+---------------------------------------------------------+--------+--------+
+         * | id  | name               | description                                             | weight | volume |
+         * +-----+--------------------+---------------------------------------------------------+--------+--------|
+         * | 101 | scooter            | Small 2-wheel scooter                                   |   3.14 |   null |
+         * | 102 | car battery        | 12V car battery                                         |    8.1 |   null |
+         * | 103 | 12-pack drill bits | 12-pack of drill bits with sizes ranging from #40 to #3 |    0.8 |   null |
+         * | 104 | hammer             | 12oz carpenter's hammer                                 |   0.75 |   null |
+         * | 105 | hammer             | 14oz carpenter's hammer                                 |  0.875 |   null |
+         * | 106 | hammer             | 18oz carpenter hammer                                   |      1 |   null |
+         * | 107 | rocks              | box of assorted rocks                                   |    5.1 |   null |
+         * | 108 | jacket             | water resistent black wind breaker                      |    0.1 |   null |
+         * | 109 | spare tire         | 24 inch spare tire                                      |   22.2 |   null |
+         * | 110 | jacket             | new water resistent white wind breaker                  |    0.5 |   null |
+         * +-----+--------------------+---------------------------------------------------------+--------+--------+
          * </pre>
          */
         String[] expected =
