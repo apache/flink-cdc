68a6c56b0386c4514b67ef57fcd5b78822119edc
zh <1063410652sky@gmail..com>
2024-03-25 16:37:51 +0800

    [cdc-cli]Add support for both new and old Flink config files in Flink CDC(#3193)

--- flink-cdc-cli/src/main/java/org/apache/flink/cdc/cli/utils/FlinkEnvironmentUtils.java
+++ flink-cdc-cli/src/main/java/org/apache/flink/cdc/cli/utils/FlinkEnvironmentUtils.java
@@ -21,6 +21,7 @@ import org.apache.flink.cdc.common.configuration.Configuration;
 import org.apache.flink.cdc.composer.flink.FlinkPipelineComposer;
 import org.apache.flink.runtime.jobgraph.SavepointRestoreSettings;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
@@ -28,10 +29,16 @@ import java.util.List;
 public class FlinkEnvironmentUtils {
 
     private static final String FLINK_CONF_DIR = "conf";
-    private static final String FLINK_CONF_FILENAME = "flink-conf.yaml";
+    private static final String OLD_FLINK_CONF_FILENAME = "flink-conf.yaml";
+    private static final String NEW_FLINK_CONF_FILENAME = "config.yaml";
 
     public static Configuration loadFlinkConfiguration(Path flinkHome) throws Exception {
-        Path flinkConfPath = flinkHome.resolve(FLINK_CONF_DIR).resolve(FLINK_CONF_FILENAME);
+        Path flinkConfPath = flinkHome.resolve(FLINK_CONF_DIR).resolve(OLD_FLINK_CONF_FILENAME);
+        // If the old version of the configuration file does not exist, then attempt to use the new
+        // version of the file name.
+        if (!Files.exists(flinkConfPath)) {
+            flinkConfPath = flinkHome.resolve(FLINK_CONF_DIR).resolve(NEW_FLINK_CONF_FILENAME);
+        }
         return ConfigurationUtils.loadMapFormattedConfig(flinkConfPath);
     }
 
