7a79e8024644569764301848bdc04d7e52cd98d4
wangjunbo <wangjunbo@qiyi.com>
2024-05-20 14:23:25 +0800

    [FLINK-35391] Bump dependency of Paimon Pipeline connector to 0.8.0

--- flink-cdc-connect/flink-cdc-pipeline-connectors/flink-cdc-pipeline-connector-paimon/src/test/java/org/apache/flink/cdc/connectors/paimon/sink/v2/PaimonSinkITCase.java
+++ flink-cdc-connect/flink-cdc-pipeline-connectors/flink-cdc-pipeline-connector-paimon/src/test/java/org/apache/flink/cdc/connectors/paimon/sink/v2/PaimonSinkITCase.java
@@ -137,6 +137,7 @@ public class PaimonSinkITCase {
                         .physicalColumn("col1", DataTypes.STRING())
                         .physicalColumn("col2", DataTypes.STRING())
                         .primaryKey("col1")
+                        .option("bucket", "1")
                         .build();
         CreateTableEvent createTableEvent = new CreateTableEvent(table1, schema);
         testEvents.add(createTableEvent);
