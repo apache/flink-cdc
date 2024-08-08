5fa85fc8f1613f51843aeaad42f66373706c3e52
yuxiqian <34335406+yuxiqian@users.noreply.github.com>
2024-08-08 14:44:46 +0800

    minor: fix transform temporal function failure when millisecond is .000

--- flink-cdc-e2e-tests/flink-cdc-pipeline-e2e-tests/src/test/java/org/apache/flink/cdc/pipeline/tests/TransformE2eITCase.java
+++ flink-cdc-e2e-tests/flink-cdc-pipeline-e2e-tests/src/test/java/org/apache/flink/cdc/pipeline/tests/TransformE2eITCase.java
@@ -813,6 +813,12 @@ public class TransformE2eITCase extends PipelineTestEnvironment {
         Assert.assertEquals(currentTimestamp, nowTimestamp);
         Assert.assertEquals(currentTimestamp, localTimestamp);
 
+        // If timestamp millisecond part is .000, it will be truncated to yyyy-MM-dd'T'HH:mm:ss
+        // format. Manually append this for the following checks.
+        if (currentTimestamp.length() == 19) {
+            currentTimestamp += ".000";
+        }
+
         Instant instant =
                 LocalDateTime.parse(
                                 currentTimestamp,
