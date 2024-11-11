422439faedef9876cef804cdb9cc809f7127454c
yuxiqian <34335406+yuxiqian@users.noreply.github.com>
2024-11-11 17:19:44 +0800

    [tests][oceanbase] Fix oceanbase test failure, possibly caused by some interactions among cases
    Signed-off-by: yuxiqian <34335406+yuxiqian@users.noreply.github.com>
    
    # Conflicts:
    #	flink-cdc-connect/flink-cdc-source-connectors/flink-connector-oceanbase-cdc/src/test/java/org/apache/flink/cdc/connectors/oceanbase/table/OceanBaseMySQLModeITCase.java

--- flink-cdc-connect/flink-cdc-source-connectors/flink-connector-oceanbase-cdc/src/test/resources/ddl/mysql/column_type_test.sql
+++ flink-cdc-connect/flink-cdc-source-connectors/flink-connector-oceanbase-cdc/src/test/resources/ddl/mysql/column_type_test.sql
@@ -17,9 +17,6 @@
 -- DATABASE:  column_type_test
 -- ----------------------------------------------------------------------------------------------------------------
 
-CREATE DATABASE IF NOT EXISTS column_type_test;
-USE column_type_test;
-
 DROP TABLE IF EXISTS full_types;
 CREATE TABLE full_types
 (
