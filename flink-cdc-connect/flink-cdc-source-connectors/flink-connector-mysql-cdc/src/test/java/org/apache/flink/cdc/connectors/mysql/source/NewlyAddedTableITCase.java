911611ec58deacb6f3c581b6c941f5652d64af60
Hang Ruan <ruanhang1993@hotmail.com>
2024-11-15 14:15:57 +0800

    fix some part

--- flink-cdc-connect/flink-cdc-source-connectors/flink-connector-mysql-cdc/src/test/java/org/apache/flink/cdc/connectors/mysql/source/NewlyAddedTableITCase.java
+++ flink-cdc-connect/flink-cdc-source-connectors/flink-connector-mysql-cdc/src/test/java/org/apache/flink/cdc/connectors/mysql/source/NewlyAddedTableITCase.java
@@ -535,7 +535,8 @@ public class NewlyAddedTableITCase extends MySqlSourceTestBase {
                         operator.getOperatorIdFuture(),
                         serializer,
                         accumulatorName,
-                        stream.getExecutionEnvironment().getCheckpointConfig());
+                        stream.getExecutionEnvironment().getCheckpointConfig(),
+                        10000L);
         return iterator;
     }
 
@@ -1182,7 +1183,7 @@ public class NewlyAddedTableITCase extends MySqlSourceTestBase {
             fetchedDataList.addAll(expectedSnapshotDataThisRound);
             waitForUpsertSinkSize("sink", fetchedDataList.size());
             assertEqualsInAnyOrder(
-                    fetchedDataList, TestValuesTableFactory.getResultsAsStringsAsStrings("sink"));
+                    fetchedDataList, TestValuesTableFactory.getResultsAsStrings("sink"));
             // step 3: make some binlog data for this round
             makeFirstPartBinlogForAddressTable(getConnection(), newlyAddedTable);
             makeSecondPartBinlogForAddressTable(getConnection(), newlyAddedTable);
@@ -1213,7 +1214,7 @@ public class NewlyAddedTableITCase extends MySqlSourceTestBase {
             // checkpoint to wait retract old record and send new record
             Thread.sleep(1000);
             assertEqualsInAnyOrder(
-                    fetchedDataList, TestValuesTableFactory.getResultsAsStringsAsStrings("sink"));
+                    fetchedDataList, TestValuesTableFactory.getResultsAsStrings("sink"));
             // step 6: trigger savepoint
             if (round != captureAddressTables.length - 1) {
                 finishedSavePointPath = triggerSavepointWithRetry(jobClient, savepointDirectory);
