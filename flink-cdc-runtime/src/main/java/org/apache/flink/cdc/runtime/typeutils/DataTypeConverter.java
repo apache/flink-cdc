5b037f8dcf4ede4d198ca445a722a896e3365bae
wenmo <32723967+aiwenmo@users.noreply.github.com>
2024-12-19 21:47:37 +0800

    Fix test error

--- flink-cdc-runtime/src/main/java/org/apache/flink/cdc/runtime/typeutils/DataTypeConverter.java
+++ flink-cdc-runtime/src/main/java/org/apache/flink/cdc/runtime/typeutils/DataTypeConverter.java
@@ -111,7 +111,7 @@ public class DataTypeConverter {
             case VARBINARY:
                 return byte[].class;
             case DECIMAL:
-                return DecimalData.class;
+                return BigDecimal.class;
             case ROW:
                 return Object.class;
             case ARRAY:
