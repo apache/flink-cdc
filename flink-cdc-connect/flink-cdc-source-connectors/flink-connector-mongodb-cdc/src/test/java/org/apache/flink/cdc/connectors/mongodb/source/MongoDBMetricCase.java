d35c9335e7d5197ffabad4682913c0d546c78525
sukang <sukang1002@gmail.com>
2025-01-22 22:37:37 +0800

    test1

---
+++ flink-cdc-connect/flink-cdc-source-connectors/flink-connector-mongodb-cdc/src/test/java/org/apache/flink/cdc/connectors/mongodb/source/MongoDBMetricCase.java
@@ -0,0 +1,10 @@
+package org.apache.flink.cdc.connectors.mongodb.table;
+
+import org.apache.flink.cdc.connectors.mongodb.source.MongoDBSourceTestBase;
+
+public class MongoDBMetricCase extends MongoDBSourceTestBase {
+    public MongoDBMetricCase(String mongoVersion) {
+        super(mongoVersion);
+    }
+
+}
