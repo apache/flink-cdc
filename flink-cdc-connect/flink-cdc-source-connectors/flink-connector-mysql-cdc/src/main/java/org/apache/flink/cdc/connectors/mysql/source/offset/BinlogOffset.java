e26a99f0e73b3f45e0746062c9f8c9270cf06019
lvyanquan <lvyanquan.lyq@alibaba-inc.com>
2025-02-07 15:13:41 +0800

    Revert "[FLINK-35600][pipeline-connector/mysql] Add timestamp for low and high watermark "
    This reverts commit 2fa215e5c45818ecc7f5d73783dfb61c1f0e4828.

--- flink-cdc-connect/flink-cdc-source-connectors/flink-connector-mysql-cdc/src/main/java/org/apache/flink/cdc/connectors/mysql/source/offset/BinlogOffset.java
+++ flink-cdc-connect/flink-cdc-source-connectors/flink-connector-mysql-cdc/src/main/java/org/apache/flink/cdc/connectors/mysql/source/offset/BinlogOffset.java
@@ -251,12 +251,7 @@ public class BinlogOffset implements Comparable<BinlogOffset>, Serializable {
         }
 
         // The completed events are the same, so compare the row number ...
-        if (this.getRestartSkipRows() != that.getRestartSkipRows()) {
-            return Long.compare(this.getRestartSkipRows(), that.getRestartSkipRows());
-        }
-
-        // The skip rows are the same, so compare the timestamp ...
-        return Long.compare(this.getTimestampSec(), that.getTimestampSec());
+        return Long.compare(this.getRestartSkipRows(), that.getRestartSkipRows());
     }
 
     public boolean isAtOrBefore(BinlogOffset that) {
