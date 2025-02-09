27e3360fc0851ba79e991d39e0e119423cdbcd15
lvyanquan <lvyanquan.lyq@alibaba-inc.com>
2025-02-09 10:52:09 +0800

    [FLINK-35600][pipeline-connector/mysql] Skip the record that is equal to maxSplitHighWatermark.

--- flink-cdc-connect/flink-cdc-source-connectors/flink-connector-mysql-cdc/src/main/java/org/apache/flink/cdc/connectors/mysql/source/offset/BinlogOffset.java
+++ flink-cdc-connect/flink-cdc-source-connectors/flink-connector-mysql-cdc/src/main/java/org/apache/flink/cdc/connectors/mysql/source/offset/BinlogOffset.java
@@ -226,6 +226,10 @@ public class BinlogOffset implements Comparable<BinlogOffset>, Serializable {
         long targetServerId = that.getServerId();
 
         if (serverId != targetServerId) {
+            // serverId of lowWaterMark and highWaterMark is always 0.
+            if (serverId == 0 || targetServerId == 0) {
+                return 0;
+            }
             // These are from different servers, and their binlog coordinates are not related. So
             // the only thing we can do
             // is compare timestamps, and we have to assume that the server timestamps can be
