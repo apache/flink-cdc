1725f342425b233e1200e9beb3c89f685ab14fc6
yuxiqian <34335406+yuxiqian@users.noreply.github.com>
2025-02-18 18:04:56 +0800

    Resolve conflicts

--- flink-cdc-connect/flink-cdc-source-connectors/flink-connector-mysql-cdc/src/test/java/org/apache/flink/cdc/connectors/mysql/table/MySqlTableSourceFactoryTest.java
+++ flink-cdc-connect/flink-cdc-source-connectors/flink-connector-mysql-cdc/src/test/java/org/apache/flink/cdc/connectors/mysql/table/MySqlTableSourceFactoryTest.java
@@ -760,7 +760,7 @@ class MySqlTableSourceFactoryTest {
     }
 
     @Test
-    public void testEnablingExperimentalOptions() {
+    void testEnablingExperimentalOptions() {
         Map<String, String> properties = getAllOptions();
         properties.put("scan.parse.online.schema.changes.enabled", "true");
         properties.put("use.legacy.json.format", "true");
@@ -799,7 +799,7 @@ class MySqlTableSourceFactoryTest {
                         true,
                         true,
                         true);
-        assertEquals(expectedSource, actualSource);
+        Assertions.assertThat(actualSource).isEqualTo(expectedSource);
     }
 
     private Map<String, String> getAllOptions() {
