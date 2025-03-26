77218abd68999efd8cf9ad46f7e830caa024fd7c
Kunni <lvyanquan.lyq@alibaba-inc.com>
2025-03-26 13:53:35 +0800

    [FLINK-37539][pipeline-connector/paimon] Replace stream with parallelStream to optimize the performance
    This closes  #3966

--- flink-cdc-connect/flink-cdc-pipeline-connectors/flink-cdc-pipeline-connector-paimon/src/main/java/org/apache/flink/cdc/connectors/paimon/sink/v2/PaimonWriter.java
+++ flink-cdc-connect/flink-cdc-pipeline-connectors/flink-cdc-pipeline-connector-paimon/src/main/java/org/apache/flink/cdc/connectors/paimon/sink/v2/PaimonWriter.java
@@ -36,9 +36,10 @@ import org.apache.paimon.memory.MemoryPoolFactory;
 import org.apache.paimon.options.Options;
 import org.apache.paimon.table.FileStoreTable;
 import org.apache.paimon.utils.ExecutorThreadFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
@@ -50,6 +51,8 @@ import java.util.stream.Collectors;
 /** A {@link Sink} to write {@link DataChangeEvent} to Paimon storage. */
 public class PaimonWriter<InputT>
         implements TwoPhaseCommittingSink.PrecommittingSinkWriter<InputT, MultiTableCommittable> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(PaimonWriter.class);
 
     // use `static` because Catalog is unSerializable.
     private static Catalog catalog;
@@ -92,22 +95,32 @@ public class PaimonWriter<InputT>
     }
 
     @Override
-    public Collection<MultiTableCommittable> prepareCommit() throws IOException {
-        List<MultiTableCommittable> committables = new ArrayList<>();
-        for (Map.Entry<Identifier, StoreSinkWrite> entry : writes.entrySet()) {
-            Identifier key = entry.getKey();
-            StoreSinkWrite write = entry.getValue();
-            boolean waitCompaction = true;
-            committables.addAll(
-                    // here we set it to lastCheckpointId+1 to
-                    // avoid prepareCommit the same checkpointId with the first round.
-                    write.prepareCommit(waitCompaction, lastCheckpointId + 1).stream()
-                            .map(
-                                    committable ->
-                                            MultiTableCommittable.fromCommittable(key, committable))
-                            .collect(Collectors.toList()));
-        }
+    public Collection<MultiTableCommittable> prepareCommit() {
+        long startTime = System.currentTimeMillis();
+        List<MultiTableCommittable> committables =
+                writes.entrySet()
+                        .parallelStream()
+                        .flatMap(
+                                entry -> {
+                                    try {
+                                        // here we set it to lastCheckpointId+1 to
+                                        // avoid prepareCommit the same checkpointId with the first
+                                        // round.
+                                        return entry.getValue()
+                                                .prepareCommit(true, lastCheckpointId + 1).stream()
+                                                .map(
+                                                        committable ->
+                                                                MultiTableCommittable
+                                                                        .fromCommittable(
+                                                                                entry.getKey(),
+                                                                                committable));
+                                    } catch (IOException e) {
+                                        throw new RuntimeException(e);
+                                    }
+                                })
+                        .collect(Collectors.toList());
         lastCheckpointId++;
+        LOG.debug("Spend {} ms to prepareCommit", System.currentTimeMillis() - startTime);
         return committables;
     }
 
