266e6ff131145745d82496c5816631e0ea348b5d
wenmo <32723967+aiwenmo@users.noreply.github.com>
2025-03-29 11:57:38 +0800

    add verifyBatchMode into test

--- flink-cdc-composer/src/test/java/org/apache/flink/cdc/composer/testsource/factory/DistributedDataSourceFactory.java
+++ flink-cdc-composer/src/test/java/org/apache/flink/cdc/composer/testsource/factory/DistributedDataSourceFactory.java
@@ -46,6 +46,9 @@ public class DistributedDataSourceFactory implements DataSourceFactory {
     }
 
     @Override
+    public void verifyBatchMode(Context context) {}
+
+    @Override
     public String identifier() {
         return IDENTIFIER;
     }
