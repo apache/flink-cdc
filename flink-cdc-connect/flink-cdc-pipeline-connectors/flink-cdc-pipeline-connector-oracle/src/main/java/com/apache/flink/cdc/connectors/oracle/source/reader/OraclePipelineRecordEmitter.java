820b72040cd0728d314d3cdce8d9d86f7ea7e286
linjc13 <linjc13@chinatelecom.cn>
2025-04-18 17:05:46 +0800

    [FLINK-36796][pipeline-connector][oracle]add oracle pipeline connector.

---
+++ flink-cdc-connect/flink-cdc-pipeline-connectors/flink-cdc-pipeline-connector-oracle/src/main/java/com/apache/flink/cdc/connectors/oracle/source/reader/OraclePipelineRecordEmitter.java
@@ -0,0 +1,90 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.apache.flink.cdc.connectors.oracle.source.reader;
+
+import org.apache.flink.api.connector.source.SourceOutput;
+import org.apache.flink.cdc.common.event.CreateTableEvent;
+import org.apache.flink.cdc.common.event.Event;
+import org.apache.flink.cdc.common.schema.Schema;
+import org.apache.flink.cdc.connectors.base.source.meta.offset.OffsetFactory;
+import org.apache.flink.cdc.connectors.base.source.meta.split.SourceSplitState;
+import org.apache.flink.cdc.connectors.base.source.reader.IncrementalSourceRecordEmitter;
+import org.apache.flink.cdc.connectors.oracle.source.config.OracleSourceConfig;
+import org.apache.flink.cdc.debezium.DebeziumDeserializationSchema;
+
+import com.apache.flink.cdc.connectors.oracle.utils.DebeziumUtils;
+import com.apache.flink.cdc.connectors.oracle.utils.OracleSchemaUtils;
+import io.debezium.jdbc.JdbcConnection;
+import io.debezium.relational.TableId;
+import org.apache.kafka.connect.source.SourceRecord;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A builder to build a SourceFunction which can read snapshot and continue to consume binlog for
+ * PostgreSQL.
+ */
+public class OraclePipelineRecordEmitter extends IncrementalSourceRecordEmitter<Event> {
+    private static final long serialVersionUID = 1L;
+    private List<String> tableList;
+    private OracleSourceConfig sourceConfig;
+    private List<CreateTableEvent> createTableEventCache = null;
+    private boolean alreadySendCreateTableForBinlogSplit = false;
+
+    public OraclePipelineRecordEmitter(
+            DebeziumDeserializationSchema<Event> debeziumDeserializationSchema,
+            boolean includeSchemaChanges,
+            OffsetFactory offsetFactory,
+            OracleSourceConfig sourceConfig) {
+        super(debeziumDeserializationSchema, null, includeSchemaChanges, offsetFactory);
+        this.tableList = sourceConfig.getTableList();
+        this.sourceConfig = sourceConfig;
+        this.createTableEventCache = new ArrayList<>();
+        try (JdbcConnection jdbc = DebeziumUtils.createOracleConnection(sourceConfig)) {
+
+            List<TableId> capturedTableIds = new ArrayList<>();
+            for (String table : tableList) {
+                TableId capturedTableId = TableId.parse(table);
+                capturedTableIds.add(capturedTableId);
+            }
+            for (TableId tableId : capturedTableIds) {
+                Schema schema = OracleSchemaUtils.getSchema(jdbc, tableId);
+                createTableEventCache.add(
+                        new CreateTableEvent(
+                                org.apache.flink.cdc.common.event.TableId.tableId(
+                                        tableId.catalog(), tableId.table()),
+                                schema));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Cannot start emitter to fetch table schema.", e);
+        }
+    }
+
+    @Override
+    protected void processElement(
+            SourceRecord element, SourceOutput<Event> output, SourceSplitState splitState)
+            throws Exception {
+        if (!alreadySendCreateTableForBinlogSplit) {
+            createTableEventCache.forEach(output::collect);
+            alreadySendCreateTableForBinlogSplit = true;
+        }
+        super.processElement(element, output, splitState);
+    }
+}
---
+++ flink-cdc-connect/flink-cdc-pipeline-connectors/flink-cdc-pipeline-connector-oracle/src/main/java/com/apache/flink/cdc/connectors/oracle/utils/OracleTypeUtils.java
@@ -0,0 +1,133 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.apache.flink.cdc.connectors.oracle.utils;
+
+import org.apache.flink.cdc.common.types.BigIntType;
+import org.apache.flink.cdc.common.types.DataType;
+import org.apache.flink.cdc.common.types.DataTypes;
+import org.apache.flink.cdc.common.types.DecimalType;
+import org.apache.flink.cdc.common.types.DoubleType;
+import org.apache.flink.cdc.common.types.FloatType;
+import org.apache.flink.cdc.common.types.LocalZonedTimestampType;
+import org.apache.flink.cdc.common.types.TimestampType;
+import org.apache.flink.cdc.common.types.VarCharType;
+import org.apache.flink.cdc.common.types.ZonedTimestampType;
+
+import io.debezium.relational.Column;
+
+/** Utilities for converting from oracle types to {@link DataType}s. */
+public class OracleTypeUtils {
+
+    /** Returns a corresponding Flink data type from a debezium {@link Column}. */
+    public static DataType fromDbzColumn(Column column) {
+        DataType dataType = convertFromColumn(column);
+        if (column.isOptional()) {
+            return dataType;
+        } else {
+            return dataType.notNull();
+        }
+    }
+
+    /**
+     * Returns a corresponding Flink data type from a debezium {@link Column} with nullable always
+     * be true.
+     */
+    private static DataType convertFromColumn(Column column) {
+        String type = column.typeName();
+        DataType dataType;
+        switch (type.toUpperCase()) {
+            case "VARCHAR2":
+            case "VARCHAR":
+            case "NVARCHAR2":
+            case "CHAR":
+            case "NCHAR":
+                dataType = new VarCharType(column.length());
+                break;
+            case "BLOB":
+            case "CLOB":
+            case "TEXT":
+                dataType = DataTypes.STRING();
+                break;
+            case "NUMBER":
+                dataType =
+                        column.length() == 0 || column.scale().get() <= 0
+                                ? new BigIntType()
+                                : new DecimalType(column.length(), column.scale().get());
+                break;
+            case "LONG":
+                dataType = new BigIntType();
+                break;
+            case "DATE":
+                dataType = new TimestampType(6);
+                break;
+            case "FLOAT":
+            case "BINARY_FLOAT":
+                dataType = new FloatType();
+                break;
+            case "BINARY_DOUBLE":
+            case "DOUBLE":
+                dataType = new DoubleType();
+                break;
+            case "TIMESTAMP(1)":
+                dataType = new TimestampType(1);
+                break;
+            case "TIMESTAMP(2)":
+                dataType = new TimestampType(2);
+                break;
+            case "TIMESTAMP(3)":
+                dataType = new TimestampType(3);
+                break;
+            case "TIMESTAMP(4)":
+                dataType = new TimestampType(4);
+                break;
+            case "TIMESTAMP(6)":
+                dataType = new TimestampType(6);
+                break;
+            case "TIMESTAMP(9)":
+                dataType = new TimestampType(9);
+                break;
+            case "TIMESTAMP(9) WITH TIME ZONE":
+                dataType = new ZonedTimestampType(9);
+                break;
+            case "TIMESTAMP(6) WITH TIME ZONE":
+                dataType = new ZonedTimestampType(6);
+                break;
+            case "TIMESTAMP(3) WITH TIME ZONE":
+                dataType = new ZonedTimestampType(3);
+                break;
+            case "TIMESTAMP(13) WITH TIME ZONE":
+                dataType = new ZonedTimestampType(13);
+                break;
+            case "TIMESTAMP(6) WITH LOCAL TIME ZONE":
+                dataType = new LocalZonedTimestampType(6);
+                break;
+            case "INTERVAL YEAR(2) TO MONTH":
+            case "INTERVAL DAY(3) TO SECOND(2)":
+                dataType = new BigIntType();
+                break;
+            case "XMLTYPE":
+                dataType = new VarCharType();
+                break;
+            default:
+                throw new RuntimeException("Unsupported data type:" + type);
+        }
+        return dataType;
+    }
+
+    private OracleTypeUtils() {}
+}
