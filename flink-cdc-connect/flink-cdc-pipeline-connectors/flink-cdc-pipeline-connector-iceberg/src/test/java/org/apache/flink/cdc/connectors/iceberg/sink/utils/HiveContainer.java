23a789c1545a58dd5121635d079ae652db323f7a
zhangchao.doovvv <zhangchao.doovvv@bytedance.com>
2025-08-07 15:24:24 +0800

    style: run mvn spotless:apply

--- flink-cdc-connect/flink-cdc-pipeline-connectors/flink-cdc-pipeline-connector-iceberg/src/test/java/org/apache/flink/cdc/connectors/iceberg/sink/utils/HiveContainer.java
+++ flink-cdc-connect/flink-cdc-pipeline-connectors/flink-cdc-pipeline-connector-iceberg/src/test/java/org/apache/flink/cdc/connectors/iceberg/sink/utils/HiveContainer.java
@@ -15,22 +15,27 @@
  * limitations under the License.
  */
 package org.apache.flink.cdc.connectors.iceberg.sink.utils;
+
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.Network;
 import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy;
 import org.testcontainers.utility.DockerImageName;
+
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
+
 /** Docker container for Apache Hive 4.0.0. */
 public class HiveContainer extends GenericContainer<HiveContainer> {
     private static final String DOCKER_IMAGE_NAME = "apache/hive:4.0.0";
     public static final int METASTORE_PORT = 9083;
     public static final int HIVESERVER2_PORT = 10000;
     public static final Network NETWORK = Network.newNetwork();
+
     public HiveContainer(String hostWarehouseDir) {
         this(NETWORK, hostWarehouseDir);
     }
+
     public HiveContainer(Network network, String hostWarehouseDir) {
         super(DockerImageName.parse(DOCKER_IMAGE_NAME));
         setExposedPorts(Arrays.asList(METASTORE_PORT, HIVESERVER2_PORT));
@@ -45,9 +50,11 @@ public class HiveContainer extends GenericContainer<HiveContainer> {
                                 .withRegEx(".*Actual binding is of type.*")
                                 .withStartupTimeout(Duration.of(20, ChronoUnit.SECONDS)));
     }
+
     public String getMetastoreUri() {
         return String.format("thrift://%s:%d", getHost(), getMappedPort(METASTORE_PORT));
     }
+
     public String getHiveJdbcUrl() {
         return String.format("jdbc:hive2://%s:%d/", getHost(), getMappedPort(HIVESERVER2_PORT));
     }
