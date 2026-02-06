e709237818fb2594870540497ed3fe3d41dce3b0
lvyanquan <lvyanquan.lyq@alibaba-inc.com>
2026-02-06 13:47:37 +0800

    Use org.apache.flink.shaded.jackson2.com.fasterxml.jackson instead of com.fasterxml.jackson.

--- flink-cdc-connect/flink-cdc-pipeline-connectors/flink-cdc-pipeline-connector-oracle/src/main/java/org/apache/flink/cdc/connectors/oracle/source/OracleEventDeserializer.java
+++ flink-cdc-connect/flink-cdc-pipeline-connectors/flink-cdc-pipeline-connector-oracle/src/main/java/org/apache/flink/cdc/connectors/oracle/source/OracleEventDeserializer.java
@@ -27,7 +27,8 @@ import org.apache.flink.cdc.debezium.event.DebeziumEventDeserializationSchema;
 import org.apache.flink.cdc.debezium.table.DebeziumChangelogMode;
 import org.apache.flink.table.data.TimestampData;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
+
 import io.debezium.data.Envelope;
 import io.debezium.data.geometry.Geometry;
 import io.debezium.relational.Tables;
