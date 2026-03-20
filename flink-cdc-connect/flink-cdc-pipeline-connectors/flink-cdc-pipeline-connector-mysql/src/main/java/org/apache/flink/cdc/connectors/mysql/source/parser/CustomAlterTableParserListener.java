04464c4e93262d9ae125a70dd0547d4ac0f15aa3
lvyanquan <lvyanquan.lyq@alibaba-inc.com>
2026-03-20 13:51:42 +0800

    Address comments.

--- flink-cdc-connect/flink-cdc-pipeline-connectors/flink-cdc-pipeline-connector-mysql/src/main/java/org/apache/flink/cdc/connectors/mysql/source/parser/CustomAlterTableParserListener.java
+++ flink-cdc-connect/flink-cdc-pipeline-connectors/flink-cdc-pipeline-connector-mysql/src/main/java/org/apache/flink/cdc/connectors/mysql/source/parser/CustomAlterTableParserListener.java
@@ -498,7 +498,7 @@ public class CustomAlterTableParserListener extends MySqlParserBaseListener {
 
     @Override
     public void exitTableOptionComment(MySqlParser.TableOptionCommentContext ctx) {
-        if (!parser.skipComments() && tableEditor.hasComment()) {
+        if (currentTable != null && !parser.skipComments() && tableEditor.hasComment()) {
             changes.add(
                     new AlterTableCommentEvent(
                             currentTable,
