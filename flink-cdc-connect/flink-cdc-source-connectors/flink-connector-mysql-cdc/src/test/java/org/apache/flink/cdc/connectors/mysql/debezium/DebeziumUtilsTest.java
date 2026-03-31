37bda5c20255339add7cbb6d0ac5c05173059ab1
Luca Occhipinti <luca.occhipinti@glovoapp.com>
2026-03-31 22:13:43 +0200

    fix: typo

--- flink-cdc-connect/flink-cdc-source-connectors/flink-connector-mysql-cdc/src/test/java/org/apache/flink/cdc/connectors/mysql/debezium/DebeziumUtilsTest.java
+++ flink-cdc-connect/flink-cdc-source-connectors/flink-connector-mysql-cdc/src/test/java/org/apache/flink/cdc/connectors/mysql/debezium/DebeziumUtilsTest.java
@@ -1,4 +1,4 @@
-flink-cdc-connect/flink-cdc-source-connectors/flink-connector-mysql-cdc/pom.xml/*
+/*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
