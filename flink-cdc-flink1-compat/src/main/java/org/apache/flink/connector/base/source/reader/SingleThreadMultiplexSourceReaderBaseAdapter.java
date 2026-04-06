57172a3c745341693226667e4326b01dacba976b
Pei Yu <125331682@qq.com>
2026-04-07 01:15:35 +0800

    add log
    Signed-off-by: Pei Yu <125331682@qq.com>

--- flink-cdc-flink1-compat/src/main/java/org/apache/flink/connector/base/source/reader/SingleThreadMultiplexSourceReaderBaseAdapter.java
+++ flink-cdc-flink1-compat/src/main/java/org/apache/flink/connector/base/source/reader/SingleThreadMultiplexSourceReaderBaseAdapter.java
@@ -6,11 +6,17 @@ import org.apache.flink.api.connector.source.util.ratelimit.RateLimiterStrategy;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.connector.base.source.reader.fetcher.SingleThreadFetcherManager;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.annotation.Nullable;
 
 public abstract class SingleThreadMultiplexSourceReaderBaseAdapter<
                 E, T, SplitT extends SourceSplit, SplitStateT>
         extends SingleThreadMultiplexSourceReaderBase<E, T, SplitT, SplitStateT> {
+
+    private static final Logger LOG =
+            LoggerFactory.getLogger(SingleThreadMultiplexSourceReaderBaseAdapter.class);
 
     public SingleThreadMultiplexSourceReaderBaseAdapter(
             SingleThreadFetcherManager<E, SplitT> splitFetcherManager,
@@ -18,7 +24,11 @@ public abstract class SingleThreadMultiplexSourceReaderBaseAdapter<
             @Nullable RecordEvaluator<T> eofRecordEvaluator,
             Configuration config,
             SourceReaderContext context,
-            RateLimiterStrategy rateLimiterStrategy) {
+            @Nullable RateLimiterStrategy rateLimiterStrategy) {
         super(splitFetcherManager, recordEmitter, eofRecordEvaluator, config, context);
+        if (null != rateLimiterStrategy) {
+            LOG.warn(
+                    "Because the runtime environment is Flink 1.x, the connector options `records.per.second` is ignored.");
+        }
     }
 }
