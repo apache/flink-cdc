b07ddbd3a6089c7b01c283c367abc11f60b927ae
Jubin Soni <jubinsoni27@gmail.com>
2026-05-22 15:48:46 -0400

    [FLINK-39407][cdcup] Scope /data mount to Paimon sink generator
    Move the cdc-data:/data volume mount from the generic Flink jobmanager and taskmanager service definitions in app.rb into Paimon.prepend_to_docker_compose_yaml, so the mount is only added when the user selects Paimon as the sink.

--- tools/cdcup/src/app.rb
+++ tools/cdcup/src/app.rb
@@ -90,7 +90,6 @@ flink_cdc_version = @prompt.select('  ️ Which Flink CDC version would you like
   'hostname' => 'jobmanager',
   'ports' => ['8081'],
   'command' => 'jobmanager',
-  'volumes' => ["#{CDC_DATA_VOLUME}:/data"],
   'environment' => {
     'FLINK_PROPERTIES' => "jobmanager.rpc.address: jobmanager\nexecution.checkpointing.interval: 3000"
   }
@@ -100,7 +99,6 @@ flink_cdc_version = @prompt.select('  ️ Which Flink CDC version would you like
   'image' => "flink:#{flink_version}-scala_2.12",
   'hostname' => 'taskmanager',
   'command' => 'taskmanager',
-  'volumes' => ["#{CDC_DATA_VOLUME}:/data"],
   'environment' => {
     'FLINK_PROPERTIES' => "jobmanager.rpc.address: jobmanager\ntaskmanager.numberOfTaskSlots: 4\nexecution.checkpointing.interval: 3000"
   }
