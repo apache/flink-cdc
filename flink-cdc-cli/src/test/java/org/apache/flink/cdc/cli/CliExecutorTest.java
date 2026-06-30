apache/flink-cdc

commit 5e04bc54ef14db9729d9c1012b680827d786f968

[FLINK-35360][cli] Support Yarn application mode for yaml job
This closes #3643.

Co-authored-by: ouyangwulink <mr.artboy@gmail.com>

flink-cdc-cli/src/main/java/org/apache/flink/cdc/cli/CliExecutor.java
@@ -19,85 +19,103 @@ package org.apache.flink.cdc.cli;
 
 import org.apache.flink.cdc.cli.parser.PipelineDefinitionParser;
 import org.apache.flink.cdc.cli.parser.YamlPipelineDefinitionParser;
-import org.apache.flink.cdc.cli.utils.ConfigurationUtils;
-import org.apache.flink.cdc.cli.utils.FlinkEnvironmentUtils;
 import org.apache.flink.cdc.common.annotation.VisibleForTesting;
 import org.apache.flink.cdc.common.configuration.Configuration;
 import org.apache.flink.cdc.composer.PipelineComposer;
 import org.apache.flink.cdc.composer.PipelineDeploymentExecutor;
 import org.apache.flink.cdc.composer.PipelineExecution;
 import org.apache.flink.cdc.composer.definition.PipelineDef;
-import org.apache.flink.cdc.composer.flink.deployment.ComposeDeploymentFactory;
-import org.apache.flink.runtime.jobgraph.SavepointRestoreSettings;
+import org.apache.flink.cdc.composer.flink.FlinkPipelineComposer;
+import org.apache.flink.cdc.composer.flink.deployment.ComposeDeployment;
+import org.apache.flink.cdc.composer.flink.deployment.K8SApplicationDeploymentExecutor;
+import org.apache.flink.cdc.composer.flink.deployment.YarnApplicationDeploymentExecutor;
+import org.apache.flink.configuration.DeploymentOptions;
+import org.apache.flink.core.fs.Path;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 import org.apache.commons.cli.CommandLine;
 
-import java.nio.file.Path;
 import java.util.List;
+
+import static org.apache.flink.cdc.composer.flink.deployment.ComposeDeployment.REMOTE;
 
 /** Executor for doing the composing and submitting logic for {@link CliFrontend}. */
 public class CliExecutor {
 
     private final Path pipelineDefPath;
-    private final Configuration flinkConfig;
+    private final org.apache.flink.configuration.Configuration flinkConfig;
     private final Configuration globalPipelineConfig;
-    private final boolean useMiniCluster;
     private final List<Path> additionalJars;
-
+    private final Path flinkHome;
     private final CommandLine commandLine;
-
     private PipelineComposer composer = null;
-
-    private final SavepointRestoreSettings savepointSettings;
 
     public CliExecutor(
             CommandLine commandLine,
             Path pipelineDefPath,
-            Configuration flinkConfig,
+            org.apache.flink.configuration.Configuration flinkConfig,
             Configuration globalPipelineConfig,
-            boolean useMiniCluster,
             List<Path> additionalJars,
-            SavepointRestoreSettings savepointSettings) {
+            Path flinkHome) {
         this.commandLine = commandLine;
         this.pipelineDefPath = pipelineDefPath;
         this.flinkConfig = flinkConfig;
         this.globalPipelineConfig = globalPipelineConfig;
-        this.useMiniCluster = useMiniCluster;
         this.additionalJars = additionalJars;
-        this.savepointSettings = savepointSettings;
+        this.flinkHome = flinkHome;
     }
 
     public PipelineExecution.ExecutionInfo run() throws Exception {
         // Create Submit Executor to deployment flink cdc job Or Run Flink CDC Job
-        boolean isDeploymentMode = ConfigurationUtils.isDeploymentMode(commandLine);
-        if (isDeploymentMode) {
-            ComposeDeploymentFactory composeDeploymentFactory = new ComposeDeploymentFactory();
-            PipelineDeploymentExecutor composeExecutor =
-                    composeDeploymentFactory.getFlinkComposeExecutor(commandLine);
-            org.apache.flink.configuration.Configuration configuration =
-                    org.apache.flink.configuration.Configuration.fromMap(flinkConfig.toMap());
-            SavepointRestoreSettings.toConfiguration(savepointSettings, configuration);
-            return composeExecutor.deploy(commandLine, configuration, additionalJars);
-        } else {
-            // Run CDC Job And Parse pipeline definition file
-            PipelineDefinitionParser pipelineDefinitionParser = new YamlPipelineDefinitionParser();
-            PipelineDef pipelineDef =
-                    pipelineDefinitionParser.parse(pipelineDefPath, globalPipelineConfig);
-            // Create composer
-            PipelineComposer composer = getComposer();
-            // Compose pipeline
-            PipelineExecution execution = composer.compose(pipelineDef);
-            // Execute or submit the pipeline
-            return execution.execute();
+        String deploymentTargetStr = getDeploymentTarget();
+        ComposeDeployment deploymentTarget =
+                ComposeDeployment.getDeploymentFromName(deploymentTargetStr);
+        switch (deploymentTarget) {
+            case KUBERNETES_APPLICATION:
+                return deployWithApplicationComposer(new K8SApplicationDeploymentExecutor());
+            case YARN_APPLICATION:
+                return deployWithApplicationComposer(new YarnApplicationDeploymentExecutor());
+            case LOCAL:
+                return deployWithComposer(FlinkPipelineComposer.ofMiniCluster());
+            case REMOTE:
+            case YARN_SESSION:
+                return deployWithComposer(
+                        FlinkPipelineComposer.ofRemoteCluster(flinkConfig, additionalJars));
+            default:
+                throw new IllegalArgumentException(
+                        String.format(
+                                "Deployment target %s is not supported", deploymentTargetStr));
         }
     }
 
-    private PipelineComposer getComposer() throws Exception {
-        if (composer == null) {
-            return FlinkEnvironmentUtils.createComposer(
-                    useMiniCluster, flinkConfig, additionalJars, savepointSettings);
-        }
-        return composer;
+    private PipelineExecution.ExecutionInfo deployWithApplicationComposer(
+            PipelineDeploymentExecutor composeExecutor) throws Exception {
+        return composeExecutor.deploy(commandLine, flinkConfig, additionalJars, flinkHome);
+    }
+
+    private PipelineExecution.ExecutionInfo deployWithComposer(PipelineComposer composer)
+            throws Exception {
+        PipelineDefinitionParser pipelineDefinitionParser = new YamlPipelineDefinitionParser();
+        PipelineDef pipelineDef =
+                pipelineDefinitionParser.parse(pipelineDefPath, globalPipelineConfig);
+        PipelineExecution execution = composer.compose(pipelineDef);
+        return execution.execute();
+    }
+
+    @VisibleForTesting
+    public PipelineExecution.ExecutionInfo deployWithNoOpComposer() throws Exception {
+        return deployWithComposer(this.composer);
+    }
+
+    // The main class for running application mode
+    public static void main(String[] args) throws Exception {
+        PipelineDefinitionParser pipelineDefinitionParser = new YamlPipelineDefinitionParser();
+        PipelineDef pipelineDef = pipelineDefinitionParser.parse(args[0], new Configuration());
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        FlinkPipelineComposer flinkPipelineComposer =
+                FlinkPipelineComposer.ofApplicationCluster(env);
+        PipelineExecution execution = flinkPipelineComposer.compose(pipelineDef);
+        execution.execute();
     }
 
     @VisibleForTesting
@@ -106,7 +124,7 @@ public class CliExecutor {
     }
 
     @VisibleForTesting
-    public Configuration getFlinkConfig() {
+    public org.apache.flink.configuration.Configuration getFlinkConfig() {
         return flinkConfig;
     }
 
@@ -120,12 +138,7 @@ public class CliExecutor {
         return additionalJars;
     }
 
-    @VisibleForTesting
     public String getDeploymentTarget() {
-        return commandLine.getOptionValue("target");
-    }
-
-    public SavepointRestoreSettings getSavepointSettings() {
-        return savepointSettings;
+        return flinkConfig.get(DeploymentOptions.TARGET);
     }
 }

flink-cdc-cli/pom.xml
@@ -63,6 +63,13 @@ limitations under the License.
             <scope>provided</scope>
         </dependency>
 
+        <dependency>
+            <groupId>org.apache.flink</groupId>
+            <artifactId>flink-yarn</artifactId>
+            <version>${flink.version}</version>
+            <scope>provided</scope>
+        </dependency>
+
     </dependencies>
 
 </project>

flink-cdc-cli/src/main/java/org/apache/flink/cdc/cli/CliFrontend.java
@@ -25,8 +25,12 @@ import org.apache.flink.cdc.common.configuration.ConfigOptions;
 import org.apache.flink.cdc.common.configuration.Configuration;
 import org.apache.flink.cdc.common.utils.StringUtils;
 import org.apache.flink.cdc.composer.PipelineExecution;
+import org.apache.flink.configuration.DeploymentOptions;
+import org.apache.flink.core.fs.Path;
 import org.apache.flink.runtime.jobgraph.SavepointConfigOptions;
 import org.apache.flink.runtime.jobgraph.SavepointRestoreSettings;
+
+import org.apache.flink.shaded.guava31.com.google.common.base.Joiner;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -36,9 +40,8 @@ import org.apache.commons.cli.Options;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.lang.reflect.InvocationTargetException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -49,6 +52,10 @@ import static org.apache.flink.cdc.cli.CliFrontendOptions.FLINK_CONFIG;
 import static org.apache.flink.cdc.cli.CliFrontendOptions.SAVEPOINT_ALLOW_NON_RESTORED_OPTION;
 import static org.apache.flink.cdc.cli.CliFrontendOptions.SAVEPOINT_CLAIM_MODE;
 import static org.apache.flink.cdc.cli.CliFrontendOptions.SAVEPOINT_PATH_OPTION;
+import static org.apache.flink.cdc.cli.CliFrontendOptions.TARGET;
+import static org.apache.flink.cdc.cli.CliFrontendOptions.USE_MINI_CLUSTER;
+import static org.apache.flink.cdc.composer.flink.deployment.ComposeDeployment.LOCAL;
+import static org.apache.flink.cdc.composer.flink.deployment.ComposeDeployment.REMOTE;
 
 /** The frontend entrypoint for the command-line interface of Flink CDC. */
 public class CliFrontend {
@@ -86,7 +93,7 @@ public class CliFrontend {
                     "Missing pipeline definition file path in arguments. ");
         }
 
-        Path pipelineDefPath = Paths.get(unparsedArgs.get(0));
+        Path pipelineDefPath = new Path(unparsedArgs.get(0));
         // Take the first unparsed argument as the pipeline definition file
         LOG.info("Real Path pipelineDefPath {}", pipelineDefPath);
         // Global pipeline configuration
@@ -94,13 +101,17 @@ public class CliFrontend {
 
         // Load Flink environment
         Path flinkHome = getFlinkHome(commandLine);
-        Configuration flinkConfig = FlinkEnvironmentUtils.loadFlinkConfiguration(flinkHome);
+        Configuration configuration = FlinkEnvironmentUtils.loadFlinkConfiguration(flinkHome);
 
         // To override the Flink configuration
-        overrideFlinkConfiguration(flinkConfig, commandLine);
+        overrideFlinkConfiguration(configuration, commandLine);
+
+        org.apache.flink.configuration.Configuration flinkConfig =
+                org.apache.flink.configuration.Configuration.fromMap(configuration.toMap());
 
         // Savepoint
         SavepointRestoreSettings savepointSettings = createSavepointRestoreSettings(commandLine);
+        SavepointRestoreSettings.toConfiguration(savepointSettings, flinkConfig);
 
         // Additional JARs
         List<Path> additionalJars =
@@ -108,7 +119,7 @@ public class CliFrontend {
                                 Optional.ofNullable(
                                                 commandLine.getOptionValues(CliFrontendOptions.JAR))
                                         .orElse(new String[0]))
-                        .map(Paths::get)
+                        .map(Path::new)
                         .collect(Collectors.toList());
 
         // Build executor
@@ -117,13 +128,21 @@ public class CliFrontend {
                 pipelineDefPath,
                 flinkConfig,
                 globalPipelineConfig,
-                commandLine.hasOption(CliFrontendOptions.USE_MINI_CLUSTER),
                 additionalJars,
-                savepointSettings);
+                flinkHome);
     }
 
     private static void overrideFlinkConfiguration(
             Configuration flinkConfig, CommandLine commandLine) {
+
+        String target =
+                commandLine.hasOption(USE_MINI_CLUSTER)
+                        ? LOCAL.getName()
+                        : commandLine.getOptionValue(TARGET, REMOTE.getName());
+        flinkConfig.set(
+                ConfigOptions.key(DeploymentOptions.TARGET.key()).stringType().defaultValue(target),
+                target);
+
         Properties properties = commandLine.getOptionProperties(FLINK_CONFIG.getOpt());
         LOG.info("Dynamic flink config items found: {}", properties);
         for (String key : properties.stringPropertyNames()) {
@@ -194,14 +213,14 @@ public class CliFrontend {
         String flinkHomeFromArgs = commandLine.getOptionValue(CliFrontendOptions.FLINK_HOME);
         if (flinkHomeFromArgs != null) {
             LOG.debug("Flink home is loaded by command-line argument: {}", flinkHomeFromArgs);
-            return Paths.get(flinkHomeFromArgs);
+            return new Path(flinkHomeFromArgs);
         }
 
         // Fallback to environment variable
         String flinkHomeFromEnvVar = System.getenv(FLINK_HOME_ENV_VAR);
         if (flinkHomeFromEnvVar != null) {
             LOG.debug("Flink home is loaded by environment variable: {}", flinkHomeFromEnvVar);
-            return Paths.get(flinkHomeFromEnvVar);
+            return new Path(flinkHomeFromEnvVar);
         }
 
         throw new IllegalArgumentException(
@@ -214,7 +233,7 @@ public class CliFrontend {
         // Try to get global config path from command line
         String globalConfig = commandLine.getOptionValue(CliFrontendOptions.GLOBAL_CONFIG);
         if (globalConfig != null) {
-            Path globalConfigPath = Paths.get(globalConfig);
+            Path globalConfigPath = new Path(globalConfig);
             LOG.info("Using global config in command line: {}", globalConfigPath);
             return ConfigurationUtils.loadConfigFile(globalConfigPath);
         }
@@ -223,7 +242,8 @@ public class CliFrontend {
         String flinkCdcHome = System.getenv(FLINK_CDC_HOME_ENV_VAR);
         if (flinkCdcHome != null) {
             Path globalConfigPath =
-                    Paths.get(flinkCdcHome).resolve("conf").resolve("flink-cdc.yaml");
+                    new Path(
+                            flinkCdcHome, Joiner.on(File.separator).join("conf", "flink-cdc.yaml"));
             LOG.info("Using global config in FLINK_CDC_HOME: {}", globalConfigPath);
             return ConfigurationUtils.loadConfigFile(globalConfigPath);
         }

flink-cdc-cli/src/main/java/org/apache/flink/cdc/cli/parser/PipelineDefinitionParser.java
@@ -19,8 +19,7 @@ package org.apache.flink.cdc.cli.parser;
 
 import org.apache.flink.cdc.common.configuration.Configuration;
 import org.apache.flink.cdc.composer.definition.PipelineDef;
-
-import java.nio.file.Path;
+import org.apache.flink.core.fs.Path;
 
 /** Parsing pipeline definition files and generate {@link PipelineDef}. */
 public interface PipelineDefinitionParser {

commit 0e10c043582b0944591490f37fc376dba9a1f79d

[FLINK-40005][cdc-cli] CliExecutor application mode resolves pipeline definition from local path, remote path or inline content
In application mode, CliExecutor.main received args[0] and passed it to
YamlPipelineDefinitionParser.parse(String, Configuration), which treats the
String as YAML content. The path string itself was parsed as YAML and failed
validation with: Missing required field "source" in top-level configuration.

The two native application deployment executors share CliExecutor.main but pass
args[0] with different semantics:
- K8SApplicationDeploymentExecutor sets APPLICATION_ARGS = commandLine.getArgList(),
  so args[0] is the pipeline definition FILE PATH (shipped into the JobManager
  container, e.g. mounted via a ConfigMap by the Flink Kubernetes Operator).
- YarnApplicationDeploymentExecutor reads the file on the client side and sets
  APPLICATION_ARGS to the pipeline definition CONTENT.

Resolve args[0] in three cases, in order:
1. An explicit-scheme path (s3://, hdfs://, oss://, file://) is read through
   Flink's FileSystem so the matching plugin resolves it. The scheme is explicit,
   so it is not at risk of being hijacked by the cluster default FileSystem.
2. A bare local file path is read with the local JVM file API (java.nio
   Files.readAllBytes) instead of Flink's FileSystem, whose cluster default may
   be S3/HDFS and would not resolve a local path shipped next to the JobManager.
3. Otherwise the value is already the pipeline definition content (YARN) and is
   parsed verbatim.

This fixes K8S application mode without regressing YARN application mode, and
also supports placing the pipeline definition file on remote storage.

Also update the Kubernetes deployment docs (EN + ZH): the FlinkDeployment example
now uses entryClass org.apache.flink.cdc.cli.CliExecutor without --use-mini-cluster,
and the "native application mode is not supported" note is removed.

Regression introduced by #3643 (FLINK-35360, Support Yarn application mode for

flink-cdc-cli/src/main/java/org/apache/flink/cdc/cli/CliExecutor.java
@@ -30,11 +30,20 @@ import org.apache.flink.cdc.composer.flink.deployment.ComposeDeployment;
 import org.apache.flink.cdc.composer.flink.deployment.K8SApplicationDeploymentExecutor;
 import org.apache.flink.cdc.composer.flink.deployment.YarnApplicationDeploymentExecutor;
 import org.apache.flink.configuration.DeploymentOptions;
+import org.apache.flink.core.fs.FSDataInputStream;
+import org.apache.flink.core.fs.FileSystem;
 import org.apache.flink.core.fs.Path;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 import org.apache.commons.cli.CommandLine;
 
+import java.io.ByteArrayOutputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Paths;
 import java.util.List;
 
 /** Executor for doing the composing and submitting logic for {@link CliFrontend}. */
@@ -108,12 +117,88 @@ public class CliExecutor {
     // The main class for running application mode
     public static void main(String[] args) throws Exception {
         PipelineDefinitionParser pipelineDefinitionParser = new YamlPipelineDefinitionParser();
-        PipelineDef pipelineDef = pipelineDefinitionParser.parse(args[0], new Configuration());
+        PipelineDef pipelineDef =
+                pipelineDefinitionParser.parse(resolvePipelineDef(args[0]), new Configuration());
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         FlinkPipelineComposer flinkPipelineComposer =
                 FlinkPipelineComposer.ofApplicationCluster(env);
         PipelineExecution execution = flinkPipelineComposer.compose(pipelineDef);
         execution.execute();
+    }
+
+    /**
+     * Resolves the application-mode entrypoint argument into the pipeline definition YAML content.
+     *
+     * <p>The two native application deployment executors share this single entrypoint but pass
+     * {@code args[0]} with different semantics:
+     *
+     * <ul>
+     *   <li>{@link org.apache.flink.cdc.composer.flink.deployment.K8SApplicationDeploymentExecutor}
+     *       sets {@code APPLICATION_ARGS = commandLine.getArgList()}, so {@code args[0]} is the
+     *       pipeline definition FILE PATH shipped into the JobManager container (e.g. mounted via a
+     *       ConfigMap by the Flink Kubernetes Operator).
+     *   <li>{@link
+     *       org.apache.flink.cdc.composer.flink.deployment.YarnApplicationDeploymentExecutor} reads
+     *       the file on the client side and sets {@code APPLICATION_ARGS} to the pipeline
+     *       definition CONTENT, because it does not ship the file into the YARN container.
+     * </ul>
+     *
+     * <p>Three cases are handled, in order:
+     *
+     * <ol>
+     *   <li>An explicit-scheme path (e.g. {@code s3://}, {@code hdfs://}, {@code oss://}, {@code
+     *       file://}) is read through Flink's FileSystem so the matching plugin resolves it. The
+     *       scheme is explicit, so — unlike a bare local path — it is not at risk of being hijacked
+     *       by the cluster default FileSystem.
+     *   <li>A bare local file path (e.g. shipped into the JobManager container / mounted by a
+     *       ConfigMap by the Flink Kubernetes Operator) is read with the local JVM file API rather
+     *       than Flink's FileSystem, whose cluster default may be S3/HDFS and would not resolve a
+     *       local path.
+     *   <li>Otherwise the value is already the pipeline definition CONTENT (the YARN application
+     *       executor reads the file on the client side and passes the content) and is used
+     *       verbatim. Without distinguishing these, the parser's String overload would treat a file
+     *       path as YAML content and fail with: Missing required field "source".
+     * </ol>
+     */
+    @VisibleForTesting
+    static String resolvePipelineDef(String pipelineDefPathOrContent) throws Exception {
+        // Case 1: explicit-scheme path -> read through Flink's FileSystem (plugin-aware).
+        URI uri = tryParseUri(pipelineDefPathOrContent);
+        if (uri != null && uri.getScheme() != null) {
+            Path remotePath = new Path(pipelineDefPathOrContent);
+            FileSystem fileSystem = remotePath.getFileSystem();
+            try (FSDataInputStream in = fileSystem.open(remotePath);
+                    ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = in.read(buffer)) != -1) {
+                    out.write(buffer, 0, bytesRead);
+                }
+                return new String(out.toByteArray(), StandardCharsets.UTF_8);
+            }
+        }
+
+        // Case 2: bare local file path -> read with the local JVM file API to avoid the cluster
+        // default FileSystem (which may be S3/HDFS) hijacking a local path.
+        try {
+            java.nio.file.Path localPath = Paths.get(pipelineDefPathOrContent);
+            if (Files.isRegularFile(localPath)) {
+                return new String(Files.readAllBytes(localPath), StandardCharsets.UTF_8);
+            }
+        } catch (InvalidPathException ignored) {
+            // Not a valid local path; fall through.
+        }
+
+        // Case 3: not a path -> the YARN application executor already passes the CONTENT.
+        return pipelineDefPathOrContent;
+    }
+
+    private static URI tryParseUri(String value) {
+        try {
+            return new URI(value);
+        } catch (URISyntaxException e) {
+            return null;
+        }
     }
 
     @VisibleForTesting

flink-cdc-cli/src/test/java/org/apache/flink/cdc/cli/CliExecutorTest.java
@@ -1,0 +1,120 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.cdc.cli;
+
+import org.apache.flink.cdc.cli.parser.YamlPipelineDefinitionParser;
+import org.apache.flink.cdc.common.configuration.Configuration;
+import org.apache.flink.cdc.composer.definition.PipelineDef;
+
+import org.apache.flink.shaded.guava31.com.google.common.io.Resources;
+
+import org.junit.jupiter.api.Test;
+
+import java.net.URI;
+import java.net.URL;
+import java.nio.file.Paths;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+/**
+ * Tests covering how {@link CliExecutor#main(String[])} (the application-mode entrypoint) loads the
+ * pipeline definition through {@link CliExecutor#resolvePipelineDef(String)}.
+ *
+ * <p>The two native application deployment executors share this single entrypoint but pass {@code
+ * args[0]} with different semantics, so {@code resolvePipelineDef} must handle both:
+ *
+ * <ul>
+ *   <li><b>Kubernetes</b> ({@code K8SApplicationDeploymentExecutor}) passes the pipeline definition
+ *       FILE PATH (shipped into the JobManager container, e.g. mounted by a ConfigMap), so it must
+ *       be read from the file system.
+ *   <li><b>YARN</b> ({@code YarnApplicationDeploymentExecutor}) reads the file on the client side
+ *       and passes the pipeline definition CONTENT, which must be used verbatim.
+ * </ul>
+ */
+class CliExecutorTest {
+
+    /**
+     * Kubernetes application mode: {@code args[0]} is a file path, so {@code resolvePipelineDef}
+     * reads the file content, which then parses into a valid pipeline definition.
+     */
+    @Test
+    void testResolvePipelineDefFromFilePath() throws Exception {
+        URL resource = Resources.getResource("definitions/pipeline-definition-minimized.yaml");
+        String pipelineDefPath = Paths.get(resource.toURI()).toString();
+
+        String content = CliExecutor.resolvePipelineDef(pipelineDefPath);
+        assertThat(content).contains("source:").contains("type: mysql");
+
+        PipelineDef pipelineDef =
+                new YamlPipelineDefinitionParser().parse(content, new Configuration());
+        assertThat(pipelineDef.getSource()).isNotNull();
+    }
+
+    /**
+     * Explicit-scheme path (here {@code file://}, the same code path as {@code s3://}, {@code
+     * hdfs://}, {@code oss://}): read through Flink's FileSystem so the matching plugin resolves
+     * it.
+     */
+    @Test
+    void testResolvePipelineDefFromSchemePath() throws Exception {
+        URL resource = Resources.getResource("definitions/pipeline-definition-minimized.yaml");
+        String schemePath = resource.toURI().toString();
+        assertThat(new URI(schemePath).getScheme()).isNotNull();
+
+        String content = CliExecutor.resolvePipelineDef(schemePath);
+        assertThat(content).contains("source:").contains("type: mysql");
+
+        PipelineDef pipelineDef =
+                new YamlPipelineDefinitionParser().parse(content, new Configuration());
+        assertThat(pipelineDef.getSource()).isNotNull();
+    }
+
+    /**
+     * YARN application mode: {@code args[0]} is already the pipeline definition content (read on
+     * the client side), so {@code resolvePipelineDef} returns it verbatim and it parses correctly.
+     */
+    @Test
+    void testResolvePipelineDefFromInlineContent() throws Exception {
+        String pipelineDefContent = "source:\n  type: mysql\n\nsink:\n  type: kafka\n";
+
+        String resolved = CliExecutor.resolvePipelineDef(pipelineDefContent);
+        assertThat(resolved).isEqualTo(pipelineDefContent);
+
+        PipelineDef pipelineDef =
+                new YamlPipelineDefinitionParser().parse(resolved, new Configuration());
+        assertThat(pipelineDef.getSource()).isNotNull();
+    }
+
+    /**
+     * The FLINK-40005 root cause: passing a file PATH straight to the String (content) overload
+     * makes the parser treat the path as YAML content, yielding a scalar node without a {@code
+     * source}. {@link CliExecutor#resolvePipelineDef(String)} avoids this for the Kubernetes path
+     * by reading the file first.
+     */
+    @Test
+    void testParsingFilePathAsYamlContentFails() {
+        String pipelineDefPath = "/opt/flink/config/pipeline.yaml";
+        assertThatThrownBy(
+                        () ->
+                                new YamlPipelineDefinitionParser()
+                                        .parse(pipelineDefPath, new Configuration()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Missing required field \"source\"");
+    }
+}